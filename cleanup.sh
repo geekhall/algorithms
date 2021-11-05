e92670cdffb700a154379ea18dc69b213783d88f
geekhall <174821300@qq.com>
2021-11-05 22:38:43 +0800

    add cleanup shell

---
+++ cleanup.sh
@@ -0,0 +1,16 @@
+#########################################################
+# filename : cleanup.sh
+# author   : geekhall
+# version  : v1.0.0
+# function : clean up tempory files.(.class, .o)
+#########################################################
+usage()
+{
+	echo "Usage:"
+	echo "cleanup.sh"
+	exit 2
+}
+
+work_dir=`pwd`
+
+find $work_dir -name *.class |xargs \rm -f
