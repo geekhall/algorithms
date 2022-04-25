2de736f0d3720259968e1ce4b30685ac585e76e2
geekhall <174821300@qq.com>
2022-04-25 12:29:29 +0800

    chore: add the answer to Q.18

--- leetcode/00018_4sum/Solution.ts
+++ leetcode/00018_4sum/Solution.ts
@@ -13,15 +13,38 @@
  */
 function fourSum(nums: number[], target: number): number[][] {
   nums.sort()
+  let n = nums.length
   let res: number[][] = []
-  let sumArr: number[] = []
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      sumArr.push()
+  for (let first = 0; first < n; first++) {
+    // unique first element.
+    if (first > 0 && nums[first] === nums[first - 1])
+      continue
+
+    let target_three = target - nums[first]
+    for (let second = first + 1; second < n; second++) {
+      // unique second element.
+      if (second > first + 1 && nums[second] === nums[second - 1])
+        continue
+
+      let target_two = target_three - nums[second]
+
+      for (let third = second + 1; third < n; third++) {
+        if (third > second + 1 && nums[third] === nums[third - 1])
+          continue
+        let right = n - 1
+        while (third < right) {
+          if (nums[third] + nums[right] === target_two) {
+            res.push([nums[first], nums[second], nums[third], nums[right]]);
+            break
+          }
+          right--
+        }
+      }
     }
   }
   return res
 };
+
 
 function test_00018() {
   // Input: nums = [1,0,-1,0,-2,2], target = 0
