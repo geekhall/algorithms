444b5c860f31128245d09634f4ba9586ac26f9bd
geekhall <174821300@qq.com>
2022-04-27 09:19:13 +0800

    chore: add answer to Q3

--- leetcode/02239_find-closest-number-to-zero/Solution.ts
+++ leetcode/02239_find-closest-number-to-zero/Solution.ts
@@ -1,33 +1,60 @@
-// Given an integer array nums of size n,
-// return the number with the value closest to 0 in nums.
-// If there are multiple answers,
-// return the number with the largest value.
+// Given an integer array queries and a positive integer intLength,
+// return an array answer where answer[i] is
+// either the queries[i]th smallest positive palindrome of length intLength
+// or -1 if no such palindrome exists.
 
-// Input: nums = [-4,-2,1,4,8]
-// Output: 1
-// Explanation:
-// The distance from -4 to 0 is |-4| = 4.
-// The distance from -2 to 0 is |-2| = 2.
-// The distance from 1 to 0 is |1| = 1.
-// The distance from 4 to 0 is |4| = 4.
-// The distance from 8 to 0 is |8| = 8.
-// Thus, the closest number to 0 in the array is 1.
+// A palindrome is a number that reads the same backwards and forwards.
+// Palindromes cannot have leading zeros.
 
-function findClosestNumber(nums: number[]): number {
-  if (nums == null || nums.length == 0)
-    return 0
-  let min = Number.MAX_VALUE
-  for (let i = 0; i < nums.length; i++) {
-    if (Math.abs(nums[i]) < min) {
-      min = Math.abs(nums[i])
-    }
+
+function kthPalindrome(queries: number[], intLength: number): number[] {
+  if (intLength === 1)
+    return queries.map(v => v - 1)
+
+  if (intLength === 2)
+    return queries.map(v => v + v * Math.pow(10, 1))
+
+  if (intLength === 3) {
+    return queries.map((v) => {
+      return v * Math.pow(10, intLength - 1) + v + Math.floor(v / 10) * 10
+    })
   }
-  if (nums.indexOf(min) !== -1)
-    return min
-  return -min
+  if (intLength === 4) {
+    return queries.map((v) => {
+      return v * Math.pow(10, intLength - 1) + v + Math.floor(v / 10) * Math.pow(10, intLength - 2) + Math.floor(v / 10) * Math.pow(10, intLength - 1)
+    })
+  }
+  if (intLength === 5) {
+    return queries.map((v) => {
+      return v
+    })
+  }
+  let res: number[] = []
+  let halfLength = Math.floor(intLength / 2)
+  if (intLength % 2 === 0) {
+
+  } else {
+
+  }
+
+  return res;
 };
-function test_02239() {
-  let nums = [-4, -2, -1, 4, 8]
-  console.log(findClosestNumber(nums));
+function nthPalindrome(n: number, intLength: number): number {
+
+  return 0
 }
-test_02239()
+function test_02217() {
+
+  console.log(kthPalindrome([1, 2, 3, 4, 5, 90], 3));
+  console.log(Math.pow(2, 3));
+  console.log(Math.pow(3, 4));
+  console.log(Math.pow(10, 2));
+
+  let arr = [1, 2, 3, 4, 5, 90]
+  let arr1 = arr.map(v => v - 1)
+
+  console.log(arr);
+  console.log(arr1);
+
+}
+test_02217()
