54161f449b94e7043c91de2265f43a226a679d78
geekhall <174821300@qq.com>
2022-04-27 17:29:45 +0800

    chore: add answer to Q14

---
+++ leetcode/00014_longest-common-prefix/Solution.ts
@@ -0,0 +1,12 @@
+function longestCommonPrefix(strs: string[]): string {
+  for (let i = 0; i < strs.length; i++) {
+
+  }
+  return ""
+};
+
+function test_00014() {
+  console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+  console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+}
+test_00014()
