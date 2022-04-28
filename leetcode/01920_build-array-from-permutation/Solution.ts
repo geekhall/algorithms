760e7190333672f37ea821a966dfefa70c546db0
geekhall <174821300@qq.com>
2022-04-28 15:43:04 +0800

    chore: add answer to Q1920

--- leetcode/01920_build-array-from-permutation/Solution.ts
+++ leetcode/01920_build-array-from-permutation/Solution.ts
@@ -1,8 +1,14 @@
+// Given a zero-based permutation nums (0-indexed),
+// build an array ans of the same length where ans[i] = nums[nums[i]]
+// for each 0 <= i < nums.length and return it.
+
 // A zero-based permutation nums is an array of
 // distinct integers from 0 to nums.length - 1 (inclusive).
 
 function buildArray(nums: number[]): number[] {
-
+  return Array.from({ length: nums.length }, (_, k) => {
+    return nums[nums[k]]
+  })
 };
 
 function test_01920() {
