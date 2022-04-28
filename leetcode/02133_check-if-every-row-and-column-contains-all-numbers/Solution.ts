69222a8f91c9ebb9b6fea26b8fb0611426a0e4ed
geekhall <174821300@qq.com>
2022-04-28 17:27:38 +0800

    chore: temp commit

---
+++ leetcode/02133_check-if-every-row-and-column-contains-all-numbers/Solution.ts
@@ -0,0 +1,43 @@
+// An n x n matrix is valid if every row and every column contains all the integers from 1 to n (inclusive).
+
+// Given an n x n integer matrix matrix, return true if the matrix is valid. Otherwise, return false.
+
+function checkValid(matrix: number[][]): boolean {
+  let n = matrix.length
+  for (let arr of matrix) {
+    if (!checkValidArr(arr))
+      return false
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+
+    }
+  }
+  return false
+};
+function checkValidArr(arr: number[]): boolean {
+  let s = new Set()
+  for (let num of arr) {
+    if (s.has(num))
+      return false
+  }
+  return true
+}
+function test_02133() {
+  // Input: matrix = [[1,2,3],[3,1,2],[2,3,1]]
+  // Output: true
+  // Explanation: In this case, n = 3, and every row and column contains the numbers 1, 2, and 3.
+  // Hence, we return true.
+
+  let matrix1 = [[1, 2, 3], [3, 1, 2], [2, 3, 1]]
+  console.log(checkValid(matrix1));
+
+  // Input: matrix = [[1, 1, 1], [1, 2, 3], [1, 2, 3]]
+  // Output: false
+  // Explanation: In this case, n = 3, but the first row and the first column do not contain the numbers 2 or 3.
+  // Hence, we return false.
+  let matrix2 = [[1, 1, 1], [1, 2, 3], [1, 2, 3]]
+  console.log(checkValid(matrix2));
+}
+
+test_02133()
