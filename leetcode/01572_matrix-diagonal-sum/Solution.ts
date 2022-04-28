2416bc2be1b3d2b0496b42578c8ca35436c5ebf4
geekhall <174821300@qq.com>
2022-04-28 19:58:18 +0800

    chore: add answer to Q1572

---
+++ leetcode/01572_matrix-diagonal-sum/Solution.ts
@@ -0,0 +1,43 @@
+// Given a square matrix mat, return the sum of the matrix diagonals.
+
+// Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+
+function diagonalSum(mat: number[][]): number {
+  let n = mat.length
+  let res = 0
+  mat.forEach((row, x) => {
+    row.forEach((element, y) => {
+      if (x === y || x + y === n - 1) {
+        res += element
+      }
+    })
+  })
+  return res
+};
+
+function isOdd(n: number): boolean {
+  return !(n && !(n % 2))
+}
+
+function test_01572() {
+  // Input: mat = [[1,2,3],
+  //               [4,5,6],
+  //               [7,8,9]]
+  // Output: 25
+  // Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+  // Notice that element mat[1][1] = 5 is counted only once.
+  let matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  console.log(diagonalSum(matrix1));
+
+  // Input: mat = [[1,1,1,1],
+  //             [1,1,1,1],
+  //             [1,1,1,1],
+  //             [1,1,1,1]]
+  // Output: 8
+  let matrix2 = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
+  console.log(diagonalSum(matrix2));
+}
+
+test_01572()
+
