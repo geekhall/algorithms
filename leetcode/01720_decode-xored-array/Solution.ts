7e3e19188bc64cb77df964ad2daef88180fcfc1b
geekhall <174821300@qq.com>
2022-04-30 20:54:43 +0800

    chore: add answer to Q1720

---
+++ leetcode/01720_decode-xored-array/Solution.ts
@@ -0,0 +1,44 @@
+/**
+ * ID:    01720
+ * Title: Decode XORed Array
+ * Difficulty: Easy
+ * Description: There is a hidden integer array arr that consists of n non-negative integers.
+ *
+ * It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+ *
+ * You are given the encoded array. You are also given an integer first, that is the first element of arr, i.e. arr[0].
+ *
+ * Return the original array arr. It can be proved that the answer exists and is unique.
+ *
+ * Example 1:
+ *
+ * Input: encoded = [1,2,3], first = 1 Output: [1,0,2,1] Explanation: If arr = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+ *
+ * Example 2:
+ *
+ * Input: encoded = [6,2,7,3], first = 4 Output: [4,2,0,7,4]
+ *
+ * Constraints:
+ *
+ * 2 <= n <= 10 4
+ * encoded.length == n - 1
+ * 0 <= encoded[i] <= 10 5
+ * 0 <= first <= 10 5
+ */
+function decode(encoded: number[], first: number): number[] {
+  let res: number[] = new Array(encoded.length + 1).fill(0)
+  res[0] = first
+  for (let i = 1; i < res.length; i++) {
+    res[i] = res[i - 1] ^ encoded[i - 1]
+  }
+  return res
+};
+
+function test_01720() {
+  let encoded = [1, 2, 3], first = 1
+  console.log(decode(encoded, first));
+  encoded = [6, 2, 7, 3], first = 4
+  console.log(decode(encoded, first));
+}
+
+test_01720()
