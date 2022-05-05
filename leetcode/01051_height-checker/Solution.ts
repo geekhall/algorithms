275c39dddb37361e0ea1151f352061374e855e10
geekhall <174821300@qq.com>
2022-05-05 14:17:46 +0800

    chore: add answer to 1051

---
+++ leetcode/01051_height-checker/Solution.ts
@@ -0,0 +1,44 @@
+/**
+ * ID:    01051
+ * Title: Height Checker
+ * Difficulty: Easy
+ * Description: A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the i th student in line.
+ *
+ * You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the i th student in line (0-indexed).
+ *
+ * Return the number of indices where heights[i] != expected[i].
+ *
+ * Example 1:
+ *
+ * Input: heights = [1,1,4,2,1,3] Output: 3 Explanation: heights: [1,1, 4,2, 1, 3 ] expected: [1,1, 1,2, 3, 4 ] Indices 2, 4, and 5 do not match.
+ *
+ * Example 2:
+ *
+ * Input: heights = [5,1,2,3,4] Output: 5 Explanation: heights: [ 5, 1, 2, 3, 4 ] expected: [ 1, 2, 3, 4, 5 ] All indices do not match.
+ *
+ * Example 3:
+ *
+ * Input: heights = [1,2,3,4,5] Output: 0 Explanation: heights: [1,2,3,4,5] expected: [1,2,3,4,5] All indices match.
+ *
+ * Constraints:
+ *
+ * 1 <= heights.length <= 100
+ * 1 <= heights[i] <= 100
+ */
+function heightChecker(heights: number[]): number {
+  let arr = Array.from(heights).sort((a, b) => a - b)
+  let count = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== heights[i])
+      count++
+  }
+  return count
+};
+
+function test_01051() {
+  let heights = [1, 1, 4, 2, 1, 3]
+  console.log(heightChecker(heights));
+
+}
+
+test_01051()
