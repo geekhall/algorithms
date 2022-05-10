e1e599293b6b0f097e7a375a0021b63e43ba9f39
geekhall <174821300@qq.com>
2022-05-10 17:21:31 +0800

    chore: add answers

--- leetcode/01000-01099/01021_remove-outermost-parentheses/Solution.ts
+++ leetcode/01000-01099/01021_remove-outermost-parentheses/Solution.ts
@@ -3,39 +3,57 @@
  * Title: Remove Outermost Parentheses
  * Difficulty: Easy
  * Description: A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
- * 
+ *
  * For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
- * 
+ *
  * A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
- * 
+ *
  * Given a valid parentheses string s, consider its primitive decomposition: s = P 1 + P 2 + ... + P k, where P i are primitive valid parentheses strings.
- * 
+ *
  * Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s = "(()())(())" Output:"()()()" Explanation: The input string is "(()())(())", with primitive decomposition "(()())" + "(())". After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: s = "(()())(())(()(()))" Output:"()()()()(())" Explanation: The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))". After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: s = "()()" Output:"" Explanation: The input string is "()()", with primitive decomposition "()" + "()". After removing outer parentheses of each part, this is "" + "" = "".
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= s.length <= 10 5
  * s[i] is either '(' or ')'.
  * s is a valid parentheses string.
  */
-function solution() {
-  
-}
+function removeOuterParentheses(s: string): string {
+  let st: number[] = []
+  let res = ""
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(')
+      st.push(i)
+    if (s[i] === ')') {
+      if (st.length === 1) {
+        res += s.slice(st.pop()! + 1, i)
+      } else {
+        st.pop()
+      }
+    }
+  }
+  return res
+};
 
 function test_01021() {
-  
+  let s = "(()())(())"
+  console.log(removeOuterParentheses(s));
+  s = "(()())(())(()(()))"
+  console.log(removeOuterParentheses(s));
+  s = "()()"
+  console.log(removeOuterParentheses(s));
 }
 
 test_01021()
