592e8a0fc27193bc90072cb4ec7eb59a3063e01a
geekhall <174821300@qq.com>
2022-05-11 11:05:04 +0800

    chore: add answer

--- leetcode/01400-01499/01436_destination-city/Solution.ts
+++ leetcode/01400-01499/01436_destination-city/Solution.ts
@@ -3,35 +3,63 @@
  * Title: Destination City
  * Difficulty: Easy
  * Description: You are given the array paths, where paths[i] = [cityA i, cityB i ] means there exists a direct path going from cityA i to cityB i. Return the destination city, that is, the city without any path outgoing to another city.
- * 
+ *
  * It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]] Output:"Sao Paulo" Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: paths = [["B","C"],["D","B"],["C","A"]] Output:"A" Explanation: All possible trips are: "D" -> "B" -> "C" -> "A". "B" -> "C" -> "A". "C" -> "A". "A". Clearly the destination city is "A".
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: paths = [["A","Z"]] Output:"Z"
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= paths.length <= 100
  * paths[i].length == 2
  * 1 <= cityA i.length, cityB i.length <= 10
  * cityA i!= cityB i
  * All strings consist of lowercase and uppercase English letters and the space character.
  */
-function solution() {
-  
-}
+function destCity(paths: string[][]): string {
+  let res = ""
+  let m: Map<string, number> = new Map()
+  paths.forEach((v, i) => {
+    if (m.get(v[0]) === undefined) {
+      m.set(v[0], -1)
+    } else if (m.get(v[0]) === 1) {
+      m.delete(v[0])
+    } else {
+      m.set(v[0], m.get(v[0])! - 1)
+    }
+    if (m.get(v[1]) === undefined) {
+      m.set(v[1], 1)
+    } else if (m.get(v[1]) === -1) {
+      m.delete(v[1])
+    } else {
+      m.set(v[1], m.get(v[1])! + 1)
+    }
+  })
+
+  m.forEach((v, k) => {
+    if (v === 1)
+      res = k
+  })
+  return res
+};
 
 function test_01436() {
-  
+  let paths = [["London", "New York"], ["New York", "Lima"], ["Lima", "Sao Paulo"]]
+  console.log(destCity(paths));
+  paths = [["B", "C"], ["D", "B"], ["C", "A"]]
+  console.log(destCity(paths));
+  paths = [["A", "Z"]]
+  console.log(destCity(paths));
 }
 
 test_01436()
--- leetcode/01600-01699/01624_largest-substring-between-two-equal-characters/Solution.ts
+++ leetcode/01600-01699/01624_largest-substring-between-two-equal-characters/Solution.ts
@@ -3,32 +3,48 @@
  * Title: Largest Substring Between Two Equal Characters
  * Difficulty: Easy
  * Description: Given a string s, return the length of the longest substring between two equal characters, excluding the two characters. If there is no such substring return -1.
- * 
+ *
  * A substring is a contiguous sequence of characters within a string.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s = "aa" Output: 0 Explanation: The optimal substring here is an empty substring between the two 'a's.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: s = "abca" Output: 2 Explanation: The optimal substring here is "bc".
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: s = "cbzxy" Output: -1 Explanation: There are no characters that appear twice in s.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= s.length <= 300
  * s contains only lowercase English letters.
  */
-function solution() {
-  
-}
+function maxLengthBetweenEqualCharacters(s: string): number {
+  let left = 0
+  let right = s.length - 1
+  while (s[left] !== s[right]) {
+    if (right === s.length - 1) {
+      right = right - left - 1
+      left = 0
+    } else {
+      left++
+      right++
+    }
+  }
+  return right - left - 1
+};
 
 function test_01624() {
-  
+  let s = "aa"
+  console.log(maxLengthBetweenEqualCharacters(s));
+  s = "abca"
+  console.log(maxLengthBetweenEqualCharacters(s));
+  s = "bczxcy"
+  console.log(maxLengthBetweenEqualCharacters(s));
 }
 
 test_01624()
