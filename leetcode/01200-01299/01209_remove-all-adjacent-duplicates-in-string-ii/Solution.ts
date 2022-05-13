ed78e657eedd4a12bcff1f5d734cc3dc1a5f6ff0
geekhall <174821300@qq.com>
2022-05-13 15:49:42 +0800

    chore: add answers

--- leetcode/01200-01299/01209_remove-all-adjacent-duplicates-in-string-ii/Solution.ts
+++ leetcode/01200-01299/01209_remove-all-adjacent-duplicates-in-string-ii/Solution.ts
@@ -3,35 +3,47 @@
  * Title: Remove All Adjacent Duplicates in String II
  * Difficulty: Medium
  * Description: You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
- * 
+ *
  * We repeatedly make k duplicate removals on s until we no longer can.
- * 
+ *
  * Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s = "abcd", k = 2 Output:"abcd" Explanation: There's nothing to delete.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: s = "deeedbbcccbdaa", k = 3 Output:"aa" Explanation: First delete "eee" and "ccc", get "ddbbbdaa" Then delete "bbb", get "dddaa" Finally delete "ddd", get "aa"
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: s = "pbbcggttciiippooaais", k = 2 Output:"ps"
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= s.length <= 10 5
  * 2 <= k <= 10 4
  * s only contains lower case English letters.
  */
-function solution() {
-  
-}
+function removeDuplicates(s: string, k: number): string {
+  let st: string[] = new Array()
+  for (let i = 0; i < s.length; i++) {
+    if (st.length >= k - 1 && st[st.length - 1] === s[i] && st[st.length - 2] === s[i]) {
+      for (let j = k - 1; j > 0; j--) {
+        st.pop()
+      }
+    } else {
+      st.push(s[i])
+    }
+  }
+  return st.join('')
+};
 
 function test_01209() {
-  
+  let s = "pbbcggttciiippooaais", k = 2
+  console.log(removeDuplicates(s, k));
+
 }
 
 test_01209()
--- leetcode/01400-01499/01417_reformat-the-string/Solution.ts
+++ leetcode/01400-01499/01417_reformat-the-string/Solution.ts
@@ -3,34 +3,67 @@
  * Title: Reformat The String
  * Difficulty: Easy
  * Description: You are given an alphanumeric string s. (Alphanumeric string is a string consisting of lowercase English letters and digits).
- * 
+ *
  * You have to find a permutation of the string where no letter is followed by another letter and no digit is followed by another digit. That is, no two adjacent characters have the same type.
- * 
+ *
  * Return the reformatted string or return an empty string if it is impossible to reformat the string.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s = "a0b1c2" Output:"0a1b2c" Explanation: No two adjacent characters have the same type in "0a1b2c". "a0b1c2", "0a1b2c", "0c2a1b" are also valid permutations.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: s = "leetcode" Output:"" Explanation:"leetcode" has only characters so we cannot separate them by digits.
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: s = "1229857369" Output:"" Explanation:"1229857369" has only digits so we cannot separate them by characters.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= s.length <= 500
  * s consists of only lowercase English letters and/or digits.
  */
-function solution() {
-  
-}
+function reformat(s: string): string {
+  let res = ""
+  let alphaStack = []
+  let digitStack = []
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] >= '0' && s[i] <= '9') {
+      digitStack.push(s[i])
+    } else {
+      alphaStack.push(s[i])
+    }
+  }
+  if (Math.abs(alphaStack.length - digitStack.length) > 1) {
+    return ""
+  }
+  if (alphaStack.length >= digitStack.length) {
+    for (let i = 0; i < alphaStack.length + digitStack.length; i++) {
+      if (i % 2) {
+        res += digitStack[Math.floor(i / 2)]
+      } else {
+        res += alphaStack[i / 2]
+      }
+    }
+  } else {
+    for (let i = 0; i < alphaStack.length + digitStack.length; i++) {
+      if (i % 2) {
+        res += alphaStack[Math.floor(i / 2)]
+      } else {
+        res += digitStack[i / 2]
+      }
+    }
+  }
+
+  return res
+};
 
 function test_01417() {
-  
+  let s = "a0b1c2"
+  console.log(reformat(s));
+
 }
 
 test_01417()
