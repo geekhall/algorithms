570ad1180a72ab00aa70b300232b01f965a328db
geekhall <174821300@qq.com>
2022-05-18 09:27:40 +0800

    chore: add answers

--- leetcode/00100-00199/00144_binary-tree-preorder-traversal/Solution.ts
+++ leetcode/00100-00199/00144_binary-tree-preorder-traversal/Solution.ts
@@ -3,32 +3,63 @@
  * Title: Binary Tree Preorder Traversal
  * Difficulty: Easy
  * Description: Given the root of a binary tree, return the preorder traversal of its nodes' values.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: root = [1,null,2,3] Output: [1,2,3]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: root = [] Output: []
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: root = [1] Output: [1]
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the tree is in the range [0, 100].
  * -100 <= Node.val <= 100
- * 
+ *
  * Follow up: Recursive solution is trivial, could you do it iteratively?
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode"
+
+function preorderTraversal(root: TreeNode | null): number[] {
+  if (root === null)
+    return []
+  let res = new Array()
+  // 当前节点，
+  let cur: TreeNode | null = root
+  // cur左孩子的最右节点
+  let mostRight = null
+  while (cur !== null) {
+    mostRight = cur.left
+    if (mostRight !== null) {
+      // cur 有左孩子，找到cur左子树最右节点
+      while (mostRight.right !== null && mostRight.right !== cur) {
+        mostRight = mostRight.right
+      }
+
+      if (mostRight.right === null) {// 若mostRight的右孩子指向空，让其指向cur，cur向左移动
+        mostRight.right = cur
+        res.push(cur.val)
+        cur = cur.left
+        continue
+      } else {// 若mostRight的右孩子指向cur，让其指向空，cur向右移动
+        mostRight.right = null
+        // cur = cur.right
+      }
+    } else {
+      res.push(cur.val)
+    }
+    cur = cur.right
+  }
+  return res
+};
 
 function test_00144() {
-  
+
 }
 
 test_00144()
--- leetcode/00200-00299/00257_binary-tree-paths/Solution.ts
+++ leetcode/00200-00299/00257_binary-tree-paths/Solution.ts
@@ -3,28 +3,49 @@
  * Title: Binary Tree Paths
  * Difficulty: Easy
  * Description: Given the root of a binary tree, return all root-to-leaf paths in any order.
- * 
+ *
  * A leaf is a node with no children.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: root = [1,2,3,null,5] Output: ["1->2->5","1->3"]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: root = [1] Output: ["1"]
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the tree is in the range [1, 100].
  * -100 <= Node.val <= 100
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode"
+
+function binaryTreePaths(root: TreeNode | null): string[] {
+  if (!root)
+    return []
+  let res = new Array()
+  const dfs = (node: TreeNode | null, currentPath: string | null) => {
+    if (!node)
+      return
+    if (node.left === null && node.right === null) { // is leaf
+      res.push(currentPath)
+      return
+    }
+    if (node.left)
+      dfs(node.left, currentPath + "->" + node.left!.val.toString())
+    if (node.right)
+      dfs(node.right, currentPath + "->" + node.right!.val.toString())
+  }
+  dfs(root, root!.val.toString())
+  return res
+};
 
 function test_00257() {
-  
+  let root = TreeNode.create([1, 2, 3, null, 5])
+  console.log(binaryTreePaths(root));
+  root = TreeNode.create([1])
+  console.log(binaryTreePaths(root));
 }
 
 test_00257()
