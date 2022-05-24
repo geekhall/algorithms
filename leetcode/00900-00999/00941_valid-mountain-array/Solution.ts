8c66190829318db44874e8729df8483b6675e704
geekhall <174821300@qq.com>
2022-05-24 17:29:01 +0800

    chore: add answers

--- leetcode/00900-00999/00941_valid-mountain-array/Solution.ts
+++ leetcode/00900-00999/00941_valid-mountain-array/Solution.ts
@@ -3,38 +3,61 @@
  * Title: Valid Mountain Array
  * Difficulty: Easy
  * Description: Given an array of integers arr, return true if and only if it is a valid mountain array.
- * 
+ *
  * Recall that arr is a mountain array if and only if:
- * 
+ *
  * arr.length >= 3
  * There exists some i with 0 < i < arr.length - 1 such that:
- * 
+ *
  * arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
  * arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: arr = [2,1] Output: false
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: arr = [3,5,5] Output: false
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: arr = [0,3,2,1] Output: true
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= arr.length <= 10 4
  * 0 <= arr[i] <= 10 4
  */
-function solution() {
-  
-}
+function validMountainArray(arr: number[]): boolean {
+  if (arr.length < 3)
+    return false
+  let ascendingCount = 0
+  let descendingCount = 0
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      return false
+    } else if (arr[i] > arr[i - 1]) { // asc
+      if (descendingCount > 0) {
+        return false
+      }
+      ascendingCount++
+    } else {
+      descendingCount++
+    }
+  }
+  if (descendingCount === 0 || ascendingCount === 0)
+    return false
+  return true
+};
 
 function test_00941() {
-  
+  let arr = [1, 2, 3, 4, 5]
+  console.log(validMountainArray(arr));
+  arr = [5, 4, 3, 2, 1]
+  console.log(validMountainArray(arr));
+  arr = [0, 2, 3, 4, 5, 2, 1, 0]
+  console.log(validMountainArray(arr));
 }
 
 test_00941()
--- leetcode/00900-00999/00997_find-the-town-judge/Solution.ts
+++ leetcode/00900-00999/00997_find-the-town-judge/Solution.ts
@@ -3,31 +3,31 @@
  * Title: Find the Town Judge
  * Difficulty: Easy
  * Description: In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
- * 
+ *
  * If the town judge exists, then:
- * 
+ *
  * The town judge trusts nobody.
  * Everybody (except for the town judge) trusts the town judge.
  * There is exactly one person that satisfies properties 1 and 2.
- * 
+ *
  * You are given an array trust where trust[i] = [a i, b i ] representing that the person labeled a i trusts the person labeled b i.
- * 
+ *
  * Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: n = 2, trust = [[1,2]] Output: 2
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: n = 3, trust = [[1,3],[2,3]] Output: 3
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: n = 3, trust = [[1,3],[2,3],[3,1]] Output: -1
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= n <= 1000
  * 0 <= trust.length <= 10 4
  * trust[i].length == 2
@@ -35,12 +35,12 @@
  * a i!= b i
  * 1 <= a i, b i <= n
  */
-function solution() {
-  
-}
+function findJudge(n: number, trust: number[][]): number {
+
+};
 
 function test_00997() {
-  
+
 }
 
 test_00997()
