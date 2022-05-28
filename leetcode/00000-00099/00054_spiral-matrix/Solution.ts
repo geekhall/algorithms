6aef5abf361e996bb1d448525eebbeb8eda71cc4
geekhall <174821300@qq.com>
2022-05-29 00:04:38 +0800

    chore: add answers

--- leetcode/00000-00099/00054_spiral-matrix/Solution.ts
+++ leetcode/00000-00099/00054_spiral-matrix/Solution.ts
@@ -20,18 +20,62 @@
  * -100 <= matrix[i][j] <= 100
  */
 function spiralOrder(matrix: number[][]): number[] {
+  let res = new Array()
   let x = 0
   let y = 0
-  let directions = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1]
-  ]
+  let minRow = 1
+  let maxRow = matrix.length
+  let minCol = 0
+  let maxCol = matrix[0].length
+  let total = maxCol * maxRow
+  let currentDirection = 0 // 0: right, 1: down, 2: left, 3:up
+  if (maxRow === 1) {
+    return matrix[0]
+  }
+  if (maxCol === 1) {
+    return matrix.reduce((pre, cur) => pre.concat(cur), [])
+  }
+  for (let i = 0; i < total; i++) {
+    res.push(matrix[x][y])
+    if (currentDirection === 0) {
+      y++
+      if (y === maxCol - 1) {
+        currentDirection = 1
+        maxCol--
+      }
+    }
+    else if (currentDirection === 1) {
+      x++
+      if (x === maxRow - 1) {
+        currentDirection = 2
+        maxRow--
+      }
+    } else if (currentDirection === 2) {
+      y--
+      if (y === minCol) {
+        currentDirection = 3
+        minCol++
+      }
+    } else if (currentDirection === 3) {
+      x--
+      if (x === minRow) {
+        currentDirection = 0
+        minRow++
+      }
+    }
+  }
+  return res
 };
 
 function test_00054() {
-
+  let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  console.log(spiralOrder(matrix));
+  matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+  console.log(spiralOrder(matrix));
+  matrix = [[3], [2], [1]]
+  console.log(spiralOrder(matrix));
+  matrix = [[3, 4, 5]]
+  console.log(spiralOrder(matrix));
 }
 
 test_00054()
--- leetcode/00000-00099/00059_spiral-matrix-ii/Solution.ts
+++ leetcode/00000-00099/00059_spiral-matrix-ii/Solution.ts
@@ -3,25 +3,72 @@
  * Title: Spiral Matrix II
  * Difficulty: Medium
  * Description: Given a positive integer n, generate an n x n matrix filled with elements from 1 to n 2 in spiral order.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: n = 3 Output: [[1,2,3],[8,9,4],[7,6,5]]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: n = 1 Output: [[1]]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= n <= 20
  */
-function solution() {
-  
-}
+function generateMatrix(n: number): number[][] {
+  if (n === 1)
+    return [[1]]
+  if (n === 2)
+    return [[1, 2], [4, 3]]
+  let x = 0
+  let y = 0
+  let minRow = 1
+  let maxRow = n
+  let minCol = 0
+  let maxCol = n
+  let total = maxCol * maxRow
+  let currentDirection = 0 // 0: right, 1: down, 2: left, 3:up
+
+  let matrix: number[][] = new Array(n).fill(0).map(() => new Array(n).fill(0))
+  for (let i = 1; i <= total; i++) {
+    matrix[x][y] = i
+    if (currentDirection === 0) {
+      y++
+      if (y === maxCol - 1) {
+        currentDirection = 1
+        maxCol--
+      }
+    }
+    else if (currentDirection === 1) {
+      x++
+      if (x === maxRow - 1) {
+        currentDirection = 2
+        maxRow--
+      }
+    } else if (currentDirection === 2) {
+      y--
+      if (y === minCol) {
+        currentDirection = 3
+        minCol++
+      }
+    } else if (currentDirection === 3) {
+      x--
+      if (x === minRow) {
+        currentDirection = 0
+        minRow++
+      }
+    }
+  }
+  return matrix
+};
 
 function test_00059() {
-  
+  let n = 3
+  console.log(generateMatrix(n));
+  n = 2
+  console.log(generateMatrix(n));
+
 }
 
 test_00059()
