21abd97c9f7e2a506bcf673b6f9fb03256992efd
geekhall <174821300@qq.com>
2022-05-28 10:19:47 +0800

    chore: add answers

--- leetcode/00100-00199/00114_flatten-binary-tree-to-linked-list/Solution.ts
+++ leetcode/00100-00199/00114_flatten-binary-tree-to-linked-list/Solution.ts
@@ -3,35 +3,61 @@
  * Title: Flatten Binary Tree to Linked List
  * Difficulty: Medium
  * Description: Given the root of a binary tree, flatten the tree into a "linked list":
- * 
+ *
  * The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
  * The "linked list" should be in the same order as a pre-order traversal of the binary tree.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: root = [1,2,5,3,4,null,6] Output: [1,null,2,null,3,null,4,null,5,null,6]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: root = [] Output: []
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: root = [0] Output: [0]
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the tree is in the range [0, 2000].
  * -100 <= Node.val <= 100
- * 
+ *
  * Follow up: Can you flatten the tree in-place (with O(1) extra space)?
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode"
+function flatten(root: TreeNode | null): void {
+
+  if (root === null)
+    return
+  if (root.left === null && root.right === null)
+    return
+
+  flatten(root.left)
+  flatten(root.right)
+  let tempRight = root.right
+  root.right = root.left
+  root.left = null
+  let mostRight: TreeNode | null = root
+  let pre: TreeNode | null = root
+  while (mostRight !== null) {
+    pre = mostRight
+    mostRight = mostRight.right
+  }
+  pre.right = tempRight
+};
 
 function test_00114() {
-  
+  let root = TreeNode.create([1, 2, 5, 3, 4, null, 6])
+  flatten(root)
+  TreeNode.print(root)
+  root = TreeNode.create([])
+  flatten(root)
+  TreeNode.print(root)
+  root = TreeNode.create([0])
+  flatten(root)
+  TreeNode.print(root)
 }
 
 test_00114()
--- leetcode/00900-00999/00988_smallest-string-starting-from-leaf/Solution.ts
+++ leetcode/00900-00999/00988_smallest-string-starting-from-leaf/Solution.ts
@@ -3,38 +3,87 @@
  * Title: Smallest String Starting From Leaf
  * Difficulty: Medium
  * Description: You are given the root of a binary tree where each node has a value in the range [0, 25] representing the letters 'a' to 'z'.
- * 
+ *
  * Return the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
- * 
+ *
  * As a reminder, any shorter prefix of a string is lexicographically smaller.
- * 
+ *
  * For example, "ab" is lexicographically smaller than "aba".
- * 
+ *
  * A leaf of a node is a node that has no children.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: root = [0,1,2,3,4,3,4] Output:"dba"
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: root = [25,1,3,1,3,0,2] Output:"adz"
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: root = [2,2,1,null,1,0,null,0] Output:"abc"
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the tree is in the range [1, 8500].
  * 0 <= Node.val <= 25
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode"
+// wrong answer in case [25, 1, null, 0, 0, 1, null, null, null, 0]
+function smallestFromLeaf(root: TreeNode | null): string {
 
+  if (root === null)
+    return ""
+  let min = ""
+  const dfs = (node: TreeNode | null, str: string) => {
+    if (!node)
+      return
+    str = String.fromCharCode(node.val + 97) + str
+    if (!node.left && !node.right) {
+      min = (min === "" || min > str) ? str : min
+    }
+
+    if (node.left)
+      dfs(node.left, str)
+    if (node.right)
+      dfs(node.right, str)
+  }
+  dfs(root, "")
+  return min
+}
+function smallestFromLeaf2(root: TreeNode | null): string {
+  let prev: string = "";
+  const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+  function smallest(root: TreeNode | null, prevStr: string) {
+    if (!root) return;
+    const str = `${alphabets[root.val]}${prevStr}`;
+    if (!root.left && !root.right) {
+      prev = (!prev || prev > str) ? str : prev;
+    }
+    if (root.left) {
+      smallest(root.left, str);
+    }
+    if (root.right) {
+      smallest(root.right, str);
+    }
+  }
+  smallest(root, '');
+  return prev || '';
+}
 function test_00988() {
-  
+  let root = TreeNode.create([0, 1, 2, 3, 4, 3, 4])
+  console.log(smallestFromLeaf(root));
+  root = TreeNode.create([25, 1, 3, 1, 3, 0, 2])
+  console.log(smallestFromLeaf(root));
+  root = TreeNode.create([2, 2, 1, null, 1, 0, null, 0])
+  console.log(smallestFromLeaf(root));
+
+  root = TreeNode.create([4, 0, 1, 1]) // bae
+  console.log(smallestFromLeaf(root));
+
+  root = TreeNode.create([25, 1, null, 0, 0, 1, null, null, null, 0]) // expect ababz
+  console.log(smallestFromLeaf(root));  // abz
 }
 
 test_00988()
