8fd8ab7814253067d7009e5e8d1e3307344fccd9
geekhall <174821300@qq.com>
2022-05-28 19:52:46 +0800

    chore: add answers

--- leetcode/00000-00099/00054_spiral-matrix/Solution.ts
+++ leetcode/00000-00099/00054_spiral-matrix/Solution.ts
@@ -3,28 +3,35 @@
  * Title: Spiral Matrix
  * Difficulty: Medium
  * Description: Given an m x n matrix, return all elements of the matrix in spiral order.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]] Output: [1,2,3,6,9,8,7,4,5]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]] Output: [1,2,3,4,8,12,11,10,9,5,6,7]
- * 
+ *
  * Constraints:
- * 
+ *
  * m == matrix.length
  * n == matrix[i].length
  * 1 <= m, n <= 10
  * -100 <= matrix[i][j] <= 100
  */
-function solution() {
-  
-}
+function spiralOrder(matrix: number[][]): number[] {
+  let x = 0
+  let y = 0
+  let directions = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1]
+  ]
+};
 
 function test_00054() {
-  
+
 }
 
 test_00054()
--- leetcode/01500-01599/01518_water-bottles/Solution.ts
+++ leetcode/01500-01599/01518_water-bottles/Solution.ts
@@ -3,30 +3,39 @@
  * Title: Water Bottles
  * Difficulty: Easy
  * Description: There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
- * 
+ *
  * The operation of drinking a full water bottle turns it into an empty bottle.
- * 
+ *
  * Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: numBottles = 9, numExchange = 3 Output: 13 Explanation: You can exchange 3 empty bottles to get 1 full water bottle. Number of water bottles you can drink: 9 + 3 + 1 = 13.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: numBottles = 15, numExchange = 4 Output: 19 Explanation: You can exchange 4 empty bottles to get 1 full water bottle. Number of water bottles you can drink: 15 + 3 + 1 = 19.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= numBottles <= 100
  * 2 <= numExchange <= 100
  */
-function solution() {
-  
-}
+function numWaterBottles(numBottles: number, numExchange: number): number {
+  let drinked = numBottles
+  while (numBottles >= numExchange) {
+    drinked += Math.trunc(numBottles / numExchange)
+    numBottles = Math.trunc(numBottles / numExchange) + numBottles % numExchange
+  }
+
+  return drinked
+};
 
 function test_01518() {
-  
+  let numBottles = 9, numExchange = 3
+  console.log(numWaterBottles(numBottles, numExchange));
+  numBottles = 15, numExchange = 4
+  console.log(numWaterBottles(numBottles, numExchange));
 }
 
 test_01518()
