c661332952573d9aabe06d9e6492efe65b8572a3
geekhall <174821300@qq.com>
2022-05-29 11:32:06 +0800

    chore: add answers

--- leetcode/00800-00899/00859_buddy-strings/Solution.ts
+++ leetcode/00800-00899/00859_buddy-strings/Solution.ts
@@ -3,34 +3,69 @@
  * Title: Buddy Strings
  * Difficulty: Easy
  * Description: Given two strings s and goal, return true if you can swap two letters in s so the result is equal to goal, otherwise, return false.
- * 
+ *
  * Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
- * 
+ *
  * For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s = "ab", goal = "ba" Output: true Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: s = "ab", goal = "ab" Output: false Explanation: The only letters you can swap are s[0] = 'a' and s[1] = 'b', which results in "ba" != goal.
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: s = "aa", goal = "aa" Output: true Explanation: You can swap s[0] = 'a' and s[1] = 'a' to get "aa", which is equal to goal.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= s.length, goal.length <= 2 * 10 4
  * s and goal consist of lowercase letters.
  */
-function solution() {
-  
-}
+function buddyStrings(s: string, goal: string): boolean {
+  if (s.length !== goal.length)
+    return false
+  let diffPos = new Array()
+  let m = new Map()
+  for (let i = 0; i < s.length; i++) {
+    if (m.get(s[i]) === undefined) {
+      m.set(s[i], 1)
+    } else {
+      m.set(s[i], m.get(s[i])! + 1)
+    }
+    if (s[i] !== goal[i]) {
+      diffPos.push(i)
+    }
+  }
+
+  if (diffPos.length === 2 && s[diffPos[0]] === goal[diffPos[1]] && s[diffPos[1]] === goal[diffPos[0]])
+    return true
+
+  let res = false
+  if (diffPos.length === 0) {
+    m.forEach((v, k) => {
+      if (v > 1) {
+        res = true
+        return
+      }
+    })
+  }
+  return res
+};
 
 function test_00859() {
-  
+  let s = "ab", goal = "ba"
+  console.log(buddyStrings(s, goal))
+  s = "ab", goal = "ab"
+  console.log(buddyStrings(s, goal))
+  s = "aa", goal = "aa"
+  console.log(buddyStrings(s, goal))
+
+  s = "abcaa", goal = "abcbb"
+  console.log(buddyStrings(s, goal))
 }
 
 test_00859()
--- leetcode/01700-01799/01790_check-if-one-string-swap-can-make-strings-equal/Solution.ts
+++ leetcode/01700-01799/01790_check-if-one-string-swap-can-make-strings-equal/Solution.ts
@@ -3,33 +3,50 @@
  * Title: Check if One String Swap Can Make Strings Equal
  * Difficulty: Easy
  * Description: You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
- * 
+ *
  * Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s1 = "bank", s2 = "kanb" Output: true Explanation: For example, swap the first character with the last character of s2 to make "bank".
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: s1 = "attack", s2 = "defend" Output: false Explanation: It is impossible to make them equal with one string swap.
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: s1 = "kelb", s2 = "kelb" Output: true Explanation: The two strings are already equal, so no string swap operation is required.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= s1.length, s2.length <= 100
  * s1.length == s2.length
  * s1 and s2 consist of only lowercase English letters.
  */
-function solution() {
-  
-}
+function areAlmostEqual(s1: string, s2: string): boolean {
+  if (s1.length !== s2.length)
+    return false
+  if (s1 === s2)
+    return true
+  let diffPos = new Array()
+  for (let i = 0; i < s1.length; i++) {
+
+    if (s1[i] !== s2[i]) {
+      diffPos.push(i)
+    }
+  }
+  if (diffPos.length === 2 && s1[diffPos[0]] === s2[diffPos[1]] && s1[diffPos[1]] === s2[diffPos[0]])
+    return true
+  return false
+};
 
 function test_01790() {
-  
+  let s1 = "bank", s2 = "kanb"
+  console.log(areAlmostEqual(s1, s2))
+  s1 = "attack", s2 = "defend"
+  console.log(areAlmostEqual(s1, s2))
+
 }
 
 test_01790()
