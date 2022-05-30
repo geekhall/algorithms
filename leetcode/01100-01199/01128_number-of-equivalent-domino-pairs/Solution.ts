fb5fb76a72ba74084c89880819a9cccb4da39217
geekhall <174821300@qq.com>
2022-05-30 17:34:47 +0800

    chore: add answers

--- leetcode/01100-01199/01128_number-of-equivalent-domino-pairs/Solution.ts
+++ leetcode/01100-01199/01128_number-of-equivalent-domino-pairs/Solution.ts
@@ -3,29 +3,53 @@
  * Title: Number of Equivalent Domino Pairs
  * Difficulty: Easy
  * Description: Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a == c and b == d), or (a == d and b == c) - that is, one domino can be rotated to be equal to another domino.
- * 
+ *
  * Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: dominoes = [[1,2],[2,1],[3,4],[5,6]] Output: 1
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2]] Output: 3
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= dominoes.length <= 4 * 10 4
  * dominoes[i].length == 2
  * 1 <= dominoes[i][j] <= 9
  */
-function solution() {
-  
-}
+function numEquivDominoPairs(dominoes: number[][]): number {
+  let res = 0
+  let map = new Map()
+  for (let i = 0; i < dominoes.length; i++) {
+    let key = dominoes[i][0] * 10 + dominoes[i][1] * 1
+    if (map.has(key)) {
+      map.set(key, map.get(key) + 1)
+    } else {
+      map.set(key, 1)
+    }
+    key = dominoes[i][1] * 10 + dominoes[i][0] * 1
+    if (map.has(key)) {
+      map.set(key, map.get(key) + 1)
+    } else {
+      map.set(key, 1)
+    }
+  }
+  for (let [key, value] of map) {
+    res += value * (value - 1) / 2
+  }
+  return res
+};
 
 function test_01128() {
-  
+  let dominoes = [[1, 2], [2, 1], [3, 4], [5, 6]]
+  console.log(numEquivDominoPairs(dominoes))
+  dominoes = [[1, 2], [1, 2], [1, 1], [1, 2], [2, 2]]
+  console.log(numEquivDominoPairs(dominoes))
+  dominoes = [[2, 1], [5, 4], [3, 7], [6, 2], [4, 4], [1, 8], [9, 6], [5, 3], [7, 4], [1, 9], [1, 1], [6, 6], [9, 6], [1, 3], [9, 7], [4, 7], [5, 1], [6, 5], [1, 6], [6, 1], [1, 8], [7, 2], [2, 4], [1, 6], [3, 1], [3, 9], [3, 7], [9, 1], [1, 9], [8, 9]]
+  console.log(numEquivDominoPairs(dominoes))
 }
 
 test_01128()
--- leetcode/01100-01199/01137_n-th-tribonacci-number/Solution.ts
+++ leetcode/01100-01199/01137_n-th-tribonacci-number/Solution.ts
@@ -3,30 +3,51 @@
  * Title: N-th Tribonacci Number
  * Difficulty: Easy
  * Description: The Tribonacci sequence T n is defined as follows:
- * 
+ *
  * T 0 = 0, T 1 = 1, T 2 = 1, and T n+3 = T n + T n+1 + T n+2 for n >= 0.
- * 
+ *
  * Given n, return the value of T n.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: n = 4 Output: 4 Explanation: T_3 = 0 + 1 + 1 = 2 T_4 = 1 + 1 + 2 = 4
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: n = 25 Output: 1389537
- * 
+ *
  * Constraints:
- * 
+ *
  * 0 <= n <= 37
  * The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
  */
-function solution() {
-  
-}
+function tribonacci(n: number): number {
+  if (n === 0)
+    return 0
+  if (n === 1) {
+    return 1
+  }
+  if (n === 2) {
+    return 1
+  }
+  let res = 0
+  let a = 0
+  let b = 1
+  let c = 1
+  for (let i = 2; i < n; i++) {
+    res = a + b + c
+    a = b
+    b = c
+    c = res
+  }
+
+  return res
+};
 
 function test_01137() {
-  
+  console.log(tribonacci(4))
+  console.log(tribonacci(25))
+
 }
 
 test_01137()
