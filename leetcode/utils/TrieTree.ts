b2c66f09a5bd221cab035ebc10f5064c9db62539
geekhall <174821300@qq.com>
2022-05-30 21:41:49 +0800

    chore: add trie tree

---
+++ leetcode/utils/TrieTree.ts
@@ -0,0 +1,36 @@
+class TrieNode {
+  pass: number
+  end: number
+  next: Array<TrieNode>
+  constructor() {
+    this.pass = 0
+    this.end = 0
+    this.next = new Array(26)
+  }
+}
+export class Trie {
+  root: TrieNode
+  constructor() {
+    this.root = new TrieNode()
+  }
+
+  insert(word: string) {
+    if (!word)
+      return
+    let chs = Array.from(word)
+    let node = this.root
+    node.pass++
+    let index = 0
+    for (let i = 0; i < chs.length; i++) {
+      index = chs[i].charCodeAt(0) - 'a'.charCodeAt(0)
+      if (!node.next[index]) {
+        node.next[index] = new TrieNode()
+      }
+      node = node.next[index]
+      node.pass++
+    }
+    node.end++
+  }
+
+
+}
