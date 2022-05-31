ce07af109c3b4b24087bea90ea2fa2b8dea4e23e
geekhall <174821300@qq.com>
2022-05-31 17:03:06 +0800

    chore: add answers

--- leetcode/01000-01099/01071_greatest-common-divisor-of-strings/Solution.ts
+++ leetcode/01000-01099/01071_greatest-common-divisor-of-strings/Solution.ts
@@ -3,32 +3,54 @@
  * Title: Greatest Common Divisor of Strings
  * Difficulty: Easy
  * Description: For two strings s and t, we say " t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
- * 
+ *
  * Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: str1 = "ABCABC", str2 = "ABC" Output:"ABC"
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: str1 = "ABABAB", str2 = "ABAB" Output:"AB"
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: str1 = "LEET", str2 = "CODE" Output:""
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= str1.length, str2.length <= 1000
  * str1 and str2 consist of English uppercase letters.
  */
-function solution() {
-  
-}
+function gcdOfStrings(str1: string, str2: string): string {
+  if (str1 + str2 !== str2 + str1) {
+    return ''
+  }
+  let len1 = str1.length, len2 = str2.length
+  if (len1 < len2) {
+    return gcdOfStrings(str2, str1)
+  }
+  if (len1 % len2 === 0) {
+    let i = 0
+    while (i < len1) {
+      if (str1[i] !== str2[i]) {
+        return str2.substring(0, i)
+      }
+      i++
+    }
+  } else {
+    return gcdOfStrings(str2, str1.substring(len2))
+  }
+  return str2
+};
 
 function test_01071() {
-  
+  let str1 = "ABABAB", str2 = "ABAB"
+  console.log(gcdOfStrings(str1, str2))
+  str1 = "ABCABC", str2 = "ABC"
+  console.log(gcdOfStrings(str1, str2))
+
 }
 
 test_01071()
--- leetcode/01000-01099/01089_duplicate-zeros/Solution.ts
+++ leetcode/01000-01099/01089_duplicate-zeros/Solution.ts
@@ -3,28 +3,44 @@
  * Title: Duplicate Zeros
  * Difficulty: Easy
  * Description: Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
- * 
+ *
  * Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: arr = [1,0,2,3,0,4,5,0] Output: [1,0,0,2,3,0,0,4] Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: arr = [1,2,3] Output: [1,2,3] Explanation: After calling your function, the input array is modified to: [1,2,3]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= arr.length <= 10 4
  * 0 <= arr[i] <= 9
  */
-function solution() {
-  
-}
+function duplicateZeros(arr: number[]): void {
+  let len = arr.length
+  let i = 0
+  while (i < arr.length) {
+    if (arr[i] === 0) {
+      arr.splice(i + 1, 0, 0)
+      i += 2
+    } else {
+      i++
+    }
+  }
+  while (arr.length > len) {
+    arr.pop()
+  }
+};
 
 function test_01089() {
-  
+  let arr = [1, 0, 2, 3, 0, 4, 5, 0]
+  duplicateZeros(arr)
+  console.log(arr);
+
+
 }
 
 test_01089()
