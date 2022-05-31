6d7a5340b56ca327167767b92a7426ba9385a078
geekhall <174821300@qq.com>
2022-05-31 16:25:08 +0800

    chore: add answers

--- leetcode/01100-01199/01122_relative-sort-array/Solution.ts
+++ leetcode/01100-01199/01122_relative-sort-array/Solution.ts
@@ -3,30 +3,62 @@
  * Title: Relative Sort Array
  * Difficulty: Easy
  * Description: Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
- * 
+ *
  * Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6] Output: [2,2,2,1,4,3,3,9,6,7,19]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6] Output: [22,28,8,6,17,44]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= arr1.length, arr2.length <= 1000
  * 0 <= arr1[i], arr2[i] <= 1000
  * All the elements of arr2 are distinct.
  * Each arr2[i] is in arr1.
  */
-function solution() {
-  
-}
+function relativeSortArray(arr1: number[], arr2: number[]): number[] {
+  const arr2map = (arr: number[]): Map<number, number> => {
+    let m = new Map<number, number>()
+    for (let i = 0; i < arr.length; i++) {
+      if (m.get(arr[i]) === undefined) {
+        m.set(arr[i], 1)
+      } else {
+        m.set(arr[i], m.get(arr[i])! + 1)
+      }
+    }
+    return m
+  }
+  let m = arr2map(arr1)
+  let res = new Array()
+  for (let i = 0; i < arr2.length; i++) {
+    if (m.has(arr2[i])) {
+      let v = m.get(arr2[i])!
+      while (v > 0) {
+        res.push(arr2[i])
+        v--
+      }
+      m.delete(arr2[i])
+    }
+  }
+  let remaining = new Map([...m.entries()].sort((a, b) => a[0] - b[0]))
+  remaining.forEach((v, k) => {
+    while (v > 0) {
+      res.push(k)
+      v--
+    }
+  })
+  return res
+};
 
 function test_01122() {
-  
+  let arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], arr2 = [2, 1, 4, 3, 9, 6]
+  console.log(relativeSortArray(arr1, arr2))
+
 }
 
 test_01122()
--- leetcode/01500-01599/01502_can-make-arithmetic-progression-from-sequence/Solution.ts
+++ leetcode/01500-01599/01502_can-make-arithmetic-progression-from-sequence/Solution.ts
@@ -3,28 +3,38 @@
  * Title: Can Make Arithmetic Progression From Sequence
  * Difficulty: Easy
  * Description: A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
- * 
+ *
  * Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: arr = [3,5,1] Output: true Explanation: We can reorder the elements as [1,3,5] or [5,3,1] with differences 2 and -2 respectively, between each consecutive elements.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: arr = [1,2,4] Output: false Explanation: There is no way to reorder the elements to obtain an arithmetic progression.
- * 
+ *
  * Constraints:
- * 
+ *
  * 2 <= arr.length <= 1000
  * -10 6 <= arr[i] <= 10 6
  */
-function solution() {
-  
-}
+function canMakeArithmeticProgression(arr: number[]): boolean {
+  arr.sort((a, b) => a - b)
+  let diff = arr[1] - arr[0]
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== diff) {
+      return false
+    }
+  }
+  return true
+};
 
 function test_01502() {
-  
+  let arr = [3, 5, 1]
+  console.log(canMakeArithmeticProgression(arr));
+  arr = [1, 2, 4]
+  console.log(canMakeArithmeticProgression(arr));
 }
 
 test_01502()
