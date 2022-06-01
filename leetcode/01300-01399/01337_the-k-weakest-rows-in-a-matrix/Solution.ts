e2b95cc46de32f0d66b5b7dd3b887544ad4ca25e
geekhall <174821300@qq.com>
2022-06-01 13:44:41 +0800

    chore: add answers

--- leetcode/01300-01399/01337_the-k-weakest-rows-in-a-matrix/Solution.ts
+++ leetcode/01300-01399/01337_the-k-weakest-rows-in-a-matrix/Solution.ts
@@ -3,36 +3,57 @@
  * Title: The K Weakest Rows in a Matrix
  * Difficulty: Easy
  * Description: You are given an m x n binary matrix mat of 1 's (representing soldiers) and 0 's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1 's will appear to the left of all the 0 's in each row.
- * 
+ *
  * A row i is weaker than a row j if one of the following is true:
- * 
+ *
  * The number of soldiers in row i is less than the number of soldiers in row j.
  * Both rows have the same number of soldiers and i < j.
- * 
+ *
  * Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: mat = [[1,1,0,0,0], [1,1,1,1,0], [1,0,0,0,0], [1,1,0,0,0], [1,1,1,1,1]], k = 3 Output: [2,0,3] Explanation: The number of soldiers in each row is: - Row 0: 2 - Row 1: 4 - Row 2: 1 - Row 3: 2 - Row 4: 5 The rows ordered from weakest to strongest are [2,0,3,1,4].
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: mat = [[1,0,0,0], [1,1,1,1], [1,0,0,0], [1,0,0,0]], k = 2 Output: [0,2] Explanation: The number of soldiers in each row is: - Row 0: 1 - Row 1: 4 - Row 2: 1 - Row 3: 1 The rows ordered from weakest to strongest are [0,2,3,1].
- * 
+ *
  * Constraints:
- * 
+ *
  * m == mat.length
  * n == mat[i].length
  * 2 <= n, m <= 100
  * 1 <= k <= m
  * matrix[i][j] is either 0 or 1.
  */
-function solution() {
-  
-}
+function kWeakestRows(mat: number[][], k: number): number[] {
+  const rowCount = mat.length
+  const colCount = mat[0].length
+  const rowStrength = new Map()
+  for (let i = 0; i < rowCount; i++) {
+    let strength = 0
+    for (let j = 0; j < colCount; j++) {
+      strength += mat[i][j]
+    }
+    rowStrength.set(i, strength)
+  }
+  const sortedStrength = [...rowStrength.entries()].sort((a, b) => a[1] - b[1])
+  const res = []
+  for (let i = 0; i < k; i++) {
+    res.push(sortedStrength[i][0])
+  }
+
+  return res
+};
 
 function test_01337() {
-  
+  let mat = [[1, 1, 0, 0, 0], [1, 1, 1, 1, 0], [1, 0, 0, 0, 0], [1, 1, 0, 0, 0], [1, 1, 1, 1, 1]]
+  console.log(kWeakestRows(mat, 3));
+  mat = [[1, 0, 0, 0], [1, 1, 1, 1], [1, 0, 0, 0], [1, 0, 0, 0]]
+  console.log(kWeakestRows(mat, 2));
+  mat = [[1, 0], [0, 0], [1, 0]]
+  console.log(kWeakestRows(mat, 2));  // expect [1, 0]
 }
 
 test_01337()
--- leetcode/02000-02099/02099_find-subsequence-of-length-k-with-the-largest-sum/Solution.ts
+++ leetcode/02000-02099/02099_find-subsequence-of-length-k-with-the-largest-sum/Solution.ts
@@ -3,35 +3,55 @@
  * Title: Find Subsequence of Length K With the Largest Sum
  * Difficulty: Easy
  * Description: You are given an integer array nums and an integer k. You want to find a subsequence of nums of length k that has the largest sum.
- * 
+ *
  * Return any such subsequence as an integer array of length k.
- * 
+ *
  * A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: nums = [2,1,3,3], k = 2 Output: [3,3] Explanation: The subsequence has the largest sum of 3 + 3 = 6.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: nums = [-1,-2,3,4], k = 3 Output: [-1,3,4] Explanation: The subsequence has the largest sum of -1 + 3 + 4 = 6.
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: nums = [3,4,3,3], k = 2 Output: [3,4] Explanation: The subsequence has the largest sum of 3 + 4 = 7. Another possible subsequence is [4, 3].
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= nums.length <= 1000
  * -10 5 <= nums[i] <= 10 5
  * 1 <= k <= nums.length
  */
-function solution() {
-  
-}
+function maxSubsequence(nums: number[], k: number): number[] {
+  let res: number[] = []
+  let m = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    m.set(i, nums[i])
+  }
+  let newMap = new Map([...m.entries()].sort((a, b) => b[1] - a[1]))
+  let keys = [...newMap.keys()]
+  for (let i = 0; i < k; i++) {
+    res.push(keys[i])
+  }
+
+  res.sort((a, b) => a - b)
+  return res.map(i => newMap.get(i))
+};
 
 function test_02099() {
-  
+  let nums = [2, 1, 3, 3]
+  let k = 2
+  console.log(maxSubsequence(nums, k));
+  nums = [-1, -2, 3, 4]
+  k = 3
+  console.log(maxSubsequence(nums, k));
+  nums = [3, 4, 3, 3]
+  k = 2
+  console.log(maxSubsequence(nums, k));
 }
 
 test_02099()
