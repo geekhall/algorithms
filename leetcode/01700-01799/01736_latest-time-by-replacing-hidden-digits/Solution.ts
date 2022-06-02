d46c8a93d67f8e7575dcba63b2569348acee66c4
geekhall <174821300@qq.com>
2022-06-02 20:59:39 +0800

    chore: add answers

--- leetcode/01700-01799/01736_latest-time-by-replacing-hidden-digits/Solution.ts
+++ leetcode/01700-01799/01736_latest-time-by-replacing-hidden-digits/Solution.ts
@@ -3,34 +3,69 @@
  * Title: Latest Time by Replacing Hidden Digits
  * Difficulty: Easy
  * Description: You are given a string time in the form of hh:mm, where some of the digits in the string are hidden (represented by ?).
- * 
+ *
  * The valid times are those inclusively between 00:00 and 23:59.
- * 
+ *
  * Return the latest valid time you can get from time by replacing the hidden digits.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: time = "2?:?0" Output:"23:50" Explanation: The latest hour beginning with the digit '2' is 23 and the latest minute ending with the digit '0' is 50.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: time = "0?:3?" Output:"09:39"
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: time = "1?:22" Output:"19:22"
- * 
+ *
  * Constraints:
- * 
+ *
  * time is in the format hh:mm.
  * It is guaranteed that you can produce a valid time from the given string.
  */
-function solution() {
-  
-}
+function maximumTime(time: string): string {
+  let timeArr = time.split(':')
+  let hour = timeArr[0]
+  let minute = timeArr[1]
+  let hourArr = hour.split('')
+  let minuteArr = minute.split('')
+  let maxMinuteArr = ['5', '9']
+
+  for (let i = 0; i < hourArr.length; i++) {
+    if (hourArr[0] === '?' && hourArr[1] === '?') {
+      hourArr[0] = '2'
+      hourArr[1] = '3'
+    } else if (hourArr[0] === '?') {
+      if (hourArr[1] === '0' || hourArr[1] === '1' || hourArr[1] === '2' || hourArr[1] === '3') {
+        hourArr[0] = '2'
+      } else {
+        hourArr[0] = '1'
+      }
+    } else if (hourArr[1] === '?') {
+      if (hourArr[0] === '2') {
+        hourArr[1] = '3'
+      } else {
+        hourArr[1] = '9'
+      }
+    }
+  }
+
+  for (let i = 0; i < minuteArr.length; i++) {
+    if (minuteArr[i] === '?') {
+      minuteArr[i] = maxMinuteArr[i]
+    }
+  }
+  hour = hourArr.join('')
+  minute = minuteArr.join('')
+  return hour + ':' + minute
+};
 
 function test_01736() {
-  
+  console.log(maximumTime("2?:?0"));
+  console.log(maximumTime("0?:3?"));
+  console.log(maximumTime("1?:22"));
 }
 
 test_01736()
--- leetcode/01700-01799/01742_maximum-number-of-balls-in-a-box/Solution.ts
+++ leetcode/01700-01799/01742_maximum-number-of-balls-in-a-box/Solution.ts
@@ -3,33 +3,49 @@
  * Title: Maximum Number of Balls in a Box
  * Difficulty: Easy
  * Description: You are working in a ball factory where you have n balls numbered from lowLimit up to highLimit inclusive (i.e., n == highLimit - lowLimit + 1), and an infinite number of boxes numbered from 1 to infinity.
- * 
+ *
  * Your job at this factory is to put each ball in the box with a number equal to the sum of digits of the ball's number. For example, the ball number 321 will be put in the box number 3 + 2 + 1 = 6 and the ball number 10 will be put in the box number 1 + 0 = 1.
- * 
+ *
  * Given two integers lowLimit and highLimit, return the number of balls in the box with the most balls.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: lowLimit = 1, highLimit = 10 Output: 2 Explanation: Box Number: 1 2 3 4 5 6 7 8 9 10 11 ... Ball Count: 2 1 1 1 1 1 1 1 1 0 0 ... Box 1 has the most number of balls with 2 balls.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: lowLimit = 5, highLimit = 15 Output: 2 Explanation: Box Number: 1 2 3 4 5 6 7 8 9 10 11 ... Ball Count: 1 1 1 1 2 2 1 1 1 0 0 ... Boxes 5 and 6 have the most number of balls with 2 balls in each.
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: lowLimit = 19, highLimit = 28 Output: 2 Explanation: Box Number: 1 2 3 4 5 6 7 8 9 10 11 12 ... Ball Count: 0 1 1 1 1 1 1 1 1 2 0 0 ... Box 10 has the most number of balls with 2 balls.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= lowLimit <= highLimit <= 10 5
  */
-function solution() {
-  
-}
+function countBalls(lowLimit: number, highLimit: number): number {
+  // max size is 99999 contains 5*9 and index begins from 0 so plus 1 = 46
+  const MAX_BUCKET_SIZE = 46
+  let bucket = new Array(MAX_BUCKET_SIZE).fill(0)
+  for (let i = lowLimit; i <= highLimit; i++) {
+    let count = 0
+    let num = i
+    while (num > 0) {
+      count += num % 10
+      num = Math.floor(num / 10)
+    }
+    bucket[count]++
+  }
+  bucket.sort((a, b) => b - a)
+  return bucket[0]
+};
 
 function test_01742() {
-  
+  console.log(countBalls(1, 10));
+  console.log(countBalls(5, 15));
+  console.log(countBalls(19, 28));
+  console.log(countBalls(99999, 99999));
 }
 
 test_01742()
