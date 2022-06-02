83334f2d0080fe13db07c10166f82a997612cea4
geekhall <174821300@qq.com>
2022-06-02 09:37:03 +0800

    chore: add answers

--- leetcode/01400-01499/01460_make-two-arrays-equal-by-reversing-sub-arrays/Solution.ts
+++ leetcode/01400-01499/01460_make-two-arrays-equal-by-reversing-sub-arrays/Solution.ts
@@ -3,34 +3,54 @@
  * Title: Make Two Arrays Equal by Reversing Sub-arrays
  * Difficulty: Easy
  * Description: You are given two integer arrays of equal length target and arr. In one step, you can select any non-empty sub-array of arr and reverse it. You are allowed to make any number of steps.
- * 
+ *
  * Return true if you can make arr equal to target or false otherwise.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: target = [1,2,3,4], arr = [2,4,1,3] Output: true Explanation: You can follow the next steps to convert arr to target: 1- Reverse sub-array [2,4,1], arr becomes [1,4,2,3] 2- Reverse sub-array [4,2], arr becomes [1,2,4,3] 3- Reverse sub-array [4,3], arr becomes [1,2,3,4] There are multiple ways to convert arr to target, this is not the only way to do so.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: target = [7], arr = [7] Output: true Explanation: arr is equal to target without any reverses.
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: target = [3,7,9], arr = [3,7,11] Output: false Explanation: arr does not have value 9 and it can never be converted to target.
- * 
+ *
  * Constraints:
- * 
+ *
  * target.length == arr.length
  * 1 <= target.length <= 1000
  * 1 <= target[i] <= 1000
  * 1 <= arr[i] <= 1000
  */
-function solution() {
-  
-}
+function canBeEqual(target: number[], arr: number[]): boolean {
+  const map = new Map<number, number>()
+  for (let i = 0; i < target.length; i++) {
+    if (map.has(target[i])) {
+      map.set(target[i], map.get(target[i])! + 1)
+    } else {
+      map.set(target[i], 1)
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (map.has(arr[i])) {
+      map.set(arr[i], map.get(arr[i])! - 1)
+      if (map.get(arr[i]) === 0) {
+        map.delete(arr[i])
+      }
+    } else {
+      return false
+    }
+  }
+  return map.size === 0
+};
 
 function test_01460() {
-  
+  console.log(canBeEqual([1, 2, 3, 4], [2, 4, 1, 3]));
+  console.log(canBeEqual([7], [7]));
+  console.log(canBeEqual([3, 7, 9], [3, 7, 11]));
 }
 
 test_01460()
--- leetcode/01800-01899/01837_sum-of-digits-in-base-k/Solution.ts
+++ leetcode/01800-01899/01837_sum-of-digits-in-base-k/Solution.ts
@@ -3,28 +3,34 @@
  * Title: Sum of Digits in Base K
  * Difficulty: Easy
  * Description: Given an integer n (in base 10) and a base k, return the sum of the digits of n after converting n from base 10 to base k.
- * 
+ *
  * After converting, each digit should be interpreted as a base 10 number, and the sum should be returned in base 10.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: n = 34, k = 6 Output: 9 Explanation: 34 (base 10) expressed in base 6 is 54. 5 + 4 = 9.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: n = 10, k = 10 Output: 1 Explanation: n is already in base 10. 1 + 0 = 1.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= n <= 100
  * 2 <= k <= 10
  */
-function solution() {
-  
-}
+function sumBase(n: number, k: number): number {
+  let res = 0
+  while (n > 0) {
+    res += n % k
+    n = Math.floor(n / k)
+  }
+  return res
+};
 
 function test_01837() {
-  
+  console.log(sumBase(34, 6));
+  console.log(sumBase(10, 10));
 }
 
 test_01837()
