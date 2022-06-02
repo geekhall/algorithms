ab26f3d903b903374bd730de9cc6ec27038688e6
geekhall <174821300@qq.com>
2022-06-02 08:41:04 +0800

    chore: add answers

--- leetcode/01500-01599/01544_make-the-string-great/Solution.ts
+++ leetcode/01500-01599/01544_make-the-string-great/Solution.ts
@@ -3,41 +3,60 @@
  * Title: Make The String Great
  * Difficulty: Easy
  * Description: Given a string s of lower and upper case English letters.
- * 
+ *
  * A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
- * 
+ *
  * 0 <= i <= s.length - 2
  * s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
- * 
+ *
  * To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
- * 
+ *
  * Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
- * 
+ *
  * Notice that an empty string is also good.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s = "leEeetcode" Output:"leetcode" Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: s = "abBAcC" Output:"" Explanation: We have many possible scenarios, and all lead to the same answer. For example: "abBAcC" --> "aAcC" --> "cC" --> "" "abBAcC" --> "abBA" --> "aA" --> ""
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: s = "s" Output:"s"
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= s.length <= 100
  * s contains only lower and upper case English letters.
  */
-function solution() {
-  
-}
+function makeGood(s: string): string {
+  let res = ''
+  let i = 0
+  let st = new Array()
+  while (i < s.length) {
+    if (st.length === 0 || Math.abs(s.charCodeAt(i) - st[st.length - 1].charCodeAt(0)) !== 32) {
+      st.push(s[i])
+    } else {
+      st.pop()
+    }
+    i++
+  }
+  for (let i = 0; i < st.length; i++) {
+    res += st[i]
+  }
+  return res
+};
 
 function test_01544() {
-  
+  let s = "leEeetcode"
+  console.log(makeGood(s));
+  s = "abBAcC"
+  console.log(makeGood(s));
+  s = "s"
+  console.log(makeGood(s));
 }
 
 test_01544()
--- leetcode/01500-01599/01556_thousand-separator/Solution.ts
+++ leetcode/01500-01599/01556_thousand-separator/Solution.ts
@@ -3,25 +3,38 @@
  * Title: Thousand Separator
  * Difficulty: Easy
  * Description: Given an integer n, add a dot (".") as the thousands separator and return it in string format.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: n = 987 Output:"987"
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: n = 1234 Output:"1.234"
- * 
+ *
  * Constraints:
- * 
+ *
  * 0 <= n <= 2 31 - 1
  */
-function solution() {
-  
-}
+function thousandSeparator(n: number): string {
+  if (n === 0)
+    return "0"
+  let res = ''
+  let i = 0
+  while (n > 0) {
+    if (i % 3 === 0 && i !== 0) {
+      res = '.' + res
+    }
+    res = n % 10 + res
+    n = Math.floor(n / 10)
+    i++
+  }
+  return res
+};
 
 function test_01556() {
-  
+  console.log(thousandSeparator(987));
+  console.log(thousandSeparator(1234));
 }
 
 test_01556()
