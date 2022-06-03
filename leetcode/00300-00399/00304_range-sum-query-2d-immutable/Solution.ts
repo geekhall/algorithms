2248eb2f4ebeb7fe4b5893406f7315939a1bdb32
geekhall <174821300@qq.com>
2022-06-03 09:22:48 +0800

    chore: add answers

--- leetcode/00300-00399/00304_range-sum-query-2d-immutable/Solution.ts
+++ leetcode/00300-00399/00304_range-sum-query-2d-immutable/Solution.ts
@@ -3,20 +3,20 @@
  * Title: Range Sum Query 2D - Immutable
  * Difficulty: Medium
  * Description: Given a 2D matrix matrix, handle multiple queries of the following type:
- * 
+ *
  * Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
- * 
+ *
  * Implement the NumMatrix class:
- * 
+ *
  * NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
  * int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
- * 
+ *
  * Example 1:
- * 
+ *
  * Input ["NumMatrix", "sumRegion", "sumRegion", "sumRegion"] [[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]] Output [null, 8, 11, 12] Explanation NumMatrix numMatrix = new NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]); numMatrix.sumRegion(2, 1, 4, 3); // return 8 (i.e sum of the red rectangle) numMatrix.sumRegion(1, 1, 2, 2); // return 11 (i.e sum of the green rectangle) numMatrix.sumRegion(1, 2, 2, 4); // return 12 (i.e sum of the blue rectangle)
- * 
+ *
  * Constraints:
- * 
+ *
  * m == matrix.length
  * n == matrix[i].length
  * 1 <= m, n <= 200
@@ -25,12 +25,62 @@
  * 0 <= col1 <= col2 < n
  * At most 10 4 calls will be made to sumRegion.
  */
-function solution() {
-  
+// Simple, but not efficient.
+class NumMatrix1 {
+  matrix: number[][]
+  constructor(matrix: number[][]) {
+    this.matrix = matrix
+  }
+
+  sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+    let sum = 0
+    for (let i = row1; i <= row2; i++) {
+      for (let j = col1; j <= col2; j++) {
+        sum += this.matrix[i][j]
+      }
+    }
+    return sum
+  }
 }
 
+// More efficient.
+class NumMatrix {
+  matrix: number[][]
+  constructor(matrix: number[][]) {
+    this.matrix = matrix
+
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (i === 0 && j === 0) {
+          continue
+        }
+        if (i === 0) {
+          this.matrix[i][j] += this.matrix[i][j - 1]
+        } else if (j === 0) {
+          this.matrix[i][j] += this.matrix[i - 1][j]
+        } else {
+          this.matrix[i][j] += this.matrix[i - 1][j] + this.matrix[i][j - 1] - this.matrix[i - 1][j - 1]
+        }
+      }
+    }
+  }
+
+  sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+    return this.matrix[row2][col2]
+      - (row1 > 0 ? this.matrix[row1 - 1][col2] : 0)
+      - (col1 > 0 ? this.matrix[row2][col1 - 1] : 0)
+      + (row1 > 0 && col1 > 0 ? this.matrix[row1 - 1][col1 - 1] : 0)
+  }
+}
+
+
 function test_00304() {
-  
+  let matrix = new Array([3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5])
+  let numMatrix = new NumMatrix(matrix)
+  console.log(numMatrix.sumRegion(2, 1, 4, 3))
+  console.log(numMatrix.sumRegion(1, 1, 2, 2))
+  console.log(numMatrix.sumRegion(1, 2, 2, 4))
+
 }
 
 test_00304()
--- leetcode/01800-01899/01854_maximum-population-year/Solution.ts
+++ leetcode/01800-01899/01854_maximum-population-year/Solution.ts
@@ -3,30 +3,52 @@
  * Title: Maximum Population Year
  * Difficulty: Easy
  * Description: You are given a 2D integer array logs where each logs[i] = [birth i, death i ] indicates the birth and death years of the i th person.
- * 
+ *
  * The population of some year x is the number of people alive during that year. The i th person is counted in year x 's population if x is in the inclusive range [birth i, death i - 1]. Note that the person is not counted in the year that they die.
- * 
+ *
  * Return the earliest year with the maximum population.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: logs = [[1993,1999],[2000,2010]] Output: 1993 Explanation: The maximum population is 1, and 1993 is the earliest year with this population.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: logs = [[1950,1961],[1960,1971],[1970,1981]] Output: 1960 Explanation: The maximum population is 2, and it had happened in years 1960 and 1970. The earlier year between them is 1960.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= logs.length <= 100
  * 1950 <= birth i < death i <= 2050
  */
-function solution() {
-  
-}
+function maximumPopulation(logs: number[][]): number {
+  let max = 0;
+  let map = new Map<number, number>();
+  for (let log of logs) {
+    let birth = log[0];
+    let death = log[1];
+    for (let i = birth; i < death; i++) {
+      if (map.has(i)) {
+        map.set(i, map.get(i)! + 1);
+      } else {
+        map.set(i, 1);
+      }
+    }
+    if (map.get(birth)! > max) {
+      max = map.get(birth)!;
+    }
+  }
+
+  return [...map.entries()].sort((a, b) => b[1] - a[1] === 0 ? a[0] - b[0] : b[1] - a[1])[0][0]
+};
 
 function test_01854() {
-  
+  let logs = [[1993, 1999], [2000, 2010]];
+  console.log(maximumPopulation(logs));
+  logs = [[1950, 1961], [1960, 1971], [1970, 1981]]
+  console.log(maximumPopulation(logs));
+  logs = [[2033, 2034], [2039, 2047], [1998, 2042], [2047, 2048], [2025, 2029], [2005, 2044], [1990, 1992], [1952, 1956], [1984, 2014]]
+  console.log(maximumPopulation(logs)); // expect 2005
 }
 
 test_01854()
