60c2fbb172bb3fb7df58658341cf6fc422e952fb
geekhall <174821300@qq.com>
2022-06-04 09:37:51 +0800

    chore: add answers

--- leetcode/01600-01699/01667_fix-names-in-a-table/Solution.sql
+++ leetcode/01600-01699/01667_fix-names-in-a-table/Solution.sql
@@ -3,25 +3,20 @@
  * Title: Fix Names in a Table
  * Difficulty: Easy
  * Description: Table: Users
- * 
+ *
  * +----------------+---------+ | Column Name | Type | +----------------+---------+ | user_id | int | | name | varchar | +----------------+---------+ user_id is the primary key for this table. This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
- * 
+ *
  * Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
- * 
+ *
  * Return the result table ordered by user_id.
- * 
+ *
  * The query result format is in the following example.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: Users table: +---------+-------+ | user_id | name | +---------+-------+ | 1 | aLice | | 2 | bOB | +---------+-------+ Output: +---------+-------+ | user_id | name | +---------+-------+ | 1 | Alice | | 2 | Bob | +---------+-------+
  */
-function solution() {
-  
-}
+SELECT user_id, concat(upper(substring(name,1,1)),lower(substring(name,2,length(name)-1))) as name
+FROM Users
+order by user_id
 
-function test_01667() {
-  
-}
-
-test_01667()
