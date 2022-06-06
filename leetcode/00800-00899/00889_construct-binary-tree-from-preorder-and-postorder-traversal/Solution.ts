b16b7334b9a4d2f7c3b643bd556f518a1ce38a32
geekhall <174821300@qq.com>
2022-06-07 06:00:26 +0800

    chore: add answers

--- leetcode/00800-00899/00889_construct-binary-tree-from-preorder-and-postorder-traversal/Solution.ts
+++ leetcode/00800-00899/00889_construct-binary-tree-from-preorder-and-postorder-traversal/Solution.ts
@@ -3,19 +3,19 @@
  * Title: Construct Binary Tree from Preorder and Postorder Traversal
  * Difficulty: Medium
  * Description: Given two integer arrays, preorder and postorder where preorder is the preorder traversal of a binary tree of distinct values and postorder is the postorder traversal of the same tree, reconstruct and return the binary tree.
- * 
+ *
  * If there exist multiple answers, you can return any of them.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1] Output: [1,2,3,4,5,6,7]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: preorder = [1], postorder = [1] Output: [1]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= preorder.length <= 30
  * 1 <= preorder[i] <= preorder.length
  * All the values of preorder are unique.
@@ -24,12 +24,38 @@
  * All the values of postorder are unique.
  * It is guaranteed that preorder and postorder are the preorder traversal and postorder traversal of the same binary tree.
  */
-function solution() {
-  
+import { TreeNode } from "../../utils/TreeNode";
+function constructFromPrePost(preorder: number[], postorder: number[]): TreeNode | null {
+  const postorderMap = new Map<number, number>();
+  for (let i = 0; i < postorder.length; i++) {
+    postorderMap.set(postorder[i], i);
+  }
+  let preIndex = 0;
+  const makeTree = (startIndex: number, endIndex: number): TreeNode | null => {
+    if (startIndex > endIndex) {
+      return null;
+    }
+    const node = new TreeNode(preorder[preIndex++]);
+    if (startIndex == endIndex) {
+      return node;
+    }
+
+    const postIndex = postorderMap.get(preorder[preIndex])!;
+    node.left = makeTree(startIndex, postIndex);
+    node.right = makeTree(postIndex + 1, endIndex - 1);
+    return node;
+  };
+
+  return makeTree(0, postorder.length - 1);
 }
 
 function test_00889() {
-  
+  let preorder = [1, 2, 4, 5, 3, 6, 7], postorder = [4, 5, 2, 6, 7, 3, 1]
+  let root = constructFromPrePost(preorder, postorder)
+  TreeNode.print(root);
+  preorder = [4, 2, 1, 3], postorder = [3, 1, 2, 4]
+  root = constructFromPrePost(preorder, postorder)
+  TreeNode.print(root); // expect [4,2,null,1,null,3]
 }
 
 test_00889()
--- leetcode/02100-02199/02196_create-binary-tree-from-descriptions/Solution.ts
+++ leetcode/02100-02199/02196_create-binary-tree-from-descriptions/Solution.ts
@@ -3,36 +3,67 @@
  * Title: Create Binary Tree From Descriptions
  * Difficulty: Medium
  * Description: You are given a 2D integer array descriptions where descriptions[i] = [parent i, child i, isLeft i ] indicates that parent i is the parent of child i in a binary tree of unique values. Furthermore,
- * 
+ *
  * If isLeft i == 1, then child i is the left child of parent i.
  * If isLeft i == 0, then child i is the right child of parent i.
- * 
+ *
  * Construct the binary tree described by descriptions and return its root.
- * 
+ *
  * The test cases will be generated such that the binary tree is valid.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]] Output: [50,20,80,15,17,19] Explanation: The root node is the node with value 50 since it has no parent. The resulting binary tree is shown in the diagram.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: descriptions = [[1,2,1],[2,3,0],[3,4,1]] Output: [1,2,null,null,3,4] Explanation: The root node is the node with value 1 since it has no parent. The resulting binary tree is shown in the diagram.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= descriptions.length <= 10 4
  * descriptions[i].length == 3
  * 1 <= parent i, child i <= 10 5
  * 0 <= isLeft i <= 1
  * The binary tree described by descriptions is valid.
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode"
+function createBinaryTree(descriptions: number[][]): TreeNode | null {
+  const map = new Map<number, TreeNode>()
+  const parentMap = new Map<number, number>()
+  for (const [parent, child, isLeft] of descriptions) {
+    const childNode = map.has(child) ? map.get(child)! : new TreeNode(child)
+    const parentNode = map.has(parent) ? map.get(parent)! : new TreeNode(parent)
+
+    if (isLeft) {
+      parentNode.left = childNode
+    } else {
+      parentNode.right = childNode
+    }
+    map.set(child, childNode)
+    map.set(parent, parentNode)
+    parentMap.set(child, parent)
+  }
+  let rootVal = descriptions[0][0]
+  while (parentMap.has(rootVal)) {
+    rootVal = parentMap.get(rootVal)!
+  }
+
+  return map.get(rootVal)!
+};
 
 function test_02196() {
-  
+  let descriptions = [[20, 15, 1], [20, 17, 0], [50, 20, 1], [50, 80, 0], [80, 19, 1]]
+  let root = createBinaryTree(descriptions)
+  TreeNode.print(root);
+  descriptions = [[1, 2, 1], [2, 3, 0], [3, 4, 1]]
+  root = createBinaryTree(descriptions)
+  TreeNode.print(root);
+
+  descriptions = [[39, 70, 1], [13, 39, 1], [85, 74, 1], [74, 13, 1], [38, 82, 1], [82, 85, 1]]
+  root = createBinaryTree(descriptions)
+  TreeNode.print(root); // expect [38,82,null,85,null,74,null,13,null,39,null,70]
+
 }
 
 test_02196()
