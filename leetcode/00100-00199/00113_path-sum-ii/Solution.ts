c09633aa70a748d34c89d69524bca0efbe01e505
geekhall <174821300@qq.com>
2022-06-09 22:54:10 +0800

    chore: add answers

--- leetcode/00100-00199/00113_path-sum-ii/Solution.ts
+++ leetcode/00100-00199/00113_path-sum-ii/Solution.ts
@@ -1,35 +1,57 @@
+import { TreeNode } from "../../utils/TreeNode"
+
 /**
  * ID:    00113
  * Title: Path Sum II
  * Difficulty: Medium
  * Description: Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
- * 
+ *
  * A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22 Output: [[5,4,11,2],[5,8,4,5]] Explanation: There are two paths whose sum equals targetSum: 5 + 4 + 11 + 2 = 22 5 + 8 + 4 + 5 = 22
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: root = [1,2,3], targetSum = 5 Output: []
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: root = [1,2], targetSum = 0 Output: []
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the tree is in the range [0, 5000].
  * -1000 <= Node.val <= 1000
  * -1000 <= targetSum <= 1000
  */
-function solution() {
-  
-}
+function pathSum(root: TreeNode | null, targetSum: number): number[][] {
+  let result: number[][] = []
+  if (root === null) {
+    return []
+  }
+  if (!root.left && !root.right) {
+    if (targetSum === root.val) {
+      return [[root.val]]
+    } else {
+      return []
+    }
+  }
+  if (root.left) {
+    result.push(...pathSum(root.left, targetSum - root.val).map(path => [root.val, ...path]))
+  }
+  if (root.right) {
+    result.push(...pathSum(root.right, targetSum - root.val).map(path => [root.val, ...path]))
+  }
+  return result
+};
 
 function test_00113() {
-  
+  let root = TreeNode.create([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1]), targetSum = 22
+  console.log(pathSum(root, targetSum))
+  root = TreeNode.create([1, 2, 3]), targetSum = 5
+  console.log(pathSum(root, targetSum))
 }
 
 test_00113()
