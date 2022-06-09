816eca78c6cd365a90d155d1589c64fb1348aa3e
geekhall <174821300@qq.com>
2022-06-09 22:02:19 +0800

    chore: add answers

--- leetcode/00300-00399/00341_flatten-nested-list-iterator/Solution.ts
+++ leetcode/00300-00399/00341_flatten-nested-list-iterator/Solution.ts
@@ -3,38 +3,75 @@
  * Title: Flatten Nested List Iterator
  * Difficulty: Medium
  * Description: You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists. Implement an iterator to flatten it.
- * 
+ *
  * Implement the NestedIterator class:
- * 
+ *
  * NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with the nested list nestedList.
  * int next() Returns the next integer in the nested list.
  * boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
- * 
+ *
  * Your code will be tested with the following pseudocode:
- * 
+ *
  * initialize iterator with nestedList res = [] while iterator.hasNext() append iterator.next() to the end of res return res
- * 
+ *
  * If res matches the expected flattened list, then your code will be judged as correct.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: nestedList = [[1,1],2,[1,1]] Output: [1,1,2,1,1] Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: nestedList = [1,[4,[6]]] Output: [1,4,6] Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= nestedList.length <= 500
  * The values of the integers in the nested list is in the range [-10 6, 10 6 ].
  */
-function solution() {
-  
+
+class NestedIterator {
+  static ListNode = class {
+    val: number
+    next: NestedIterator.ListNode | null
+    constructor(val: number, next: NestedIterator.ListNode | null) {
+      this.val = val
+      this.next = next
+    }
+  }
+  head: NestedIterator.ListNode | null
+
+  constructor(nestedList: NestedInteger[]) {
+    this.head = null
+
+  }
+  createLL(nestedList: NestedInteger[]) {
+    let head: Node | null = null
+    let cur: Node | null = null
+    for (let i = 0; i < nestedList.length; i++) {
+      const node = new NestedIterator.ListNode(nestedList[i].getInteger(), null)
+      if (head == null) {
+        head = node
+        cur = node
+      } else {
+        cur.next = node
+        cur = node
+      }
+    }
+    return head
+  }
+
+  hasNext(): boolean {
+
+  }
+
+  next(): number {
+
+  }
 }
 
 function test_00341() {
-  
+  let ni = new NestedIterator([[1, 1], 2, [1, 1]])
 }
 
 test_00341()
--- leetcode/01300-01399/01376_time-needed-to-inform-all-employees/Solution.ts
+++ leetcode/01300-01399/01376_time-needed-to-inform-all-employees/Solution.ts
@@ -3,25 +3,25 @@
  * Title: Time Needed to Inform All Employees
  * Difficulty: Medium
  * Description: A company has n employees with a unique ID for each employee from 0 to n - 1. The head of the company is the one with headID.
- * 
+ *
  * Each employee has one direct manager given in the manager array where manager[i] is the direct manager of the i-th employee, manager[headID] = -1. Also, it is guaranteed that the subordination relationships have a tree structure.
- * 
+ *
  * The head of the company wants to inform all the company employees of an urgent piece of news. He will inform his direct subordinates, and they will inform their subordinates, and so on until all employees know about the urgent news.
- * 
+ *
  * The i-th employee needs informTime[i] minutes to inform all of his direct subordinates (i.e., After informTime[i] minutes, all his direct subordinates can start spreading the news).
- * 
+ *
  * Return the number of minutes needed to inform all the employees about the urgent news.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: n = 1, headID = 0, manager = [-1], informTime = [0] Output: 0 Explanation: The head of the company is the only employee in the company.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: n = 6, headID = 2, manager = [2,2,-1,2,2,2], informTime = [0,0,1,0,0,0] Output: 1 Explanation: The head of the company with id = 2 is the direct manager of all the employees in the company and needs 1 minute to inform them all. The tree structure of the employees in the company is shown.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= n <= 10 5
  * 0 <= headID < n
  * manager.length == n
@@ -32,12 +32,44 @@
  * informTime[i] == 0 if employee i has no subordinates.
  * It is guaranteed that all the employees can be informed.
  */
-function solution() {
-  
-}
+function numOfMinutes(n: number, headID: number, manager: number[], informTime: number[]): number {
+  interface Node {
+    val: number
+    children: Node[]
+  }
+
+  const graph: Node[] = []
+  for (let i = 0; i < n; i++) {
+    graph[i] = { val: informTime[i], children: [] }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (manager[i] !== -1) {
+      graph[manager[i]].children.push(graph[i])
+    }
+  }
+  let max_time = 0
+  const dfs = (node: Node, current_time: number) => {
+    // console.log(`dfs ${node.val} ${current_time}`)
+    if (node.children.length === 0) {
+      max_time = Math.max(max_time, current_time)
+    }
+    for (const child of node.children) {
+      dfs(child, current_time + child.val)
+    }
+  }
+  dfs(graph[headID], informTime[headID])
+
+  return max_time
+};
 
 function test_01376() {
-  
+  let n = 1, headID = 0, manager = [-1], informTime = [0]
+  console.log(numOfMinutes(n, headID, manager, informTime))
+  n = 6, headID = 2, manager = [2, 2, -1, 2, 2, 2], informTime = [0, 0, 1, 0, 0, 0]
+  console.log(numOfMinutes(n, headID, manager, informTime))
+  n = 15, headID = 0, manager = [-1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6], informTime = [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+  console.log(numOfMinutes(n, headID, manager, informTime))
 }
 
 test_01376()
