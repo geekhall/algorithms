294a85e7966b78726e7719e6ac9357cd3d9b2dd8
geekhall <174821300@qq.com>
2022-06-09 22:33:11 +0800

    chore: add answers

--- leetcode/00100-00199/00105_construct-binary-tree-from-preorder-and-inorder-traversal/Solution.ts
+++ leetcode/00100-00199/00105_construct-binary-tree-from-preorder-and-inorder-traversal/Solution.ts
@@ -3,17 +3,17 @@
  * Title: Construct Binary Tree from Preorder and Inorder Traversal
  * Difficulty: Medium
  * Description: Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7] Output: [3,9,20,null,null,15,7]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: preorder = [-1], inorder = [-1] Output: [-1]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= preorder.length <= 3000
  * inorder.length == preorder.length
  * -3000 <= preorder[i], inorder[i] <= 3000
@@ -22,12 +22,29 @@
  * preorder is guaranteed to be the preorder traversal of the tree.
  * inorder is guaranteed to be the inorder traversal of the tree.
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode";
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+  let res = null
+  if (preorder.length === 0) {
+    return res
+  }
+  const root = preorder[0]
+  const index = inorder.indexOf(root)
+  const left = inorder.slice(0, index)
+  const right = inorder.slice(index + 1)
+  const left_preorder = preorder.slice(1, left.length + 1)
+  const right_preorder = preorder.slice(left.length + 1)
+  res = new TreeNode(root)
+  res.left = buildTree(left_preorder, left)
+  res.right = buildTree(right_preorder, right)
+
+  return res
+};
 
 function test_00105() {
-  
+  let preorder = [3, 9, 20, 15, 7], inorder = [9, 3, 15, 20, 7]
+  let res = buildTree(preorder, inorder)
+  TreeNode.print(res)
 }
 
 test_00105()
--- leetcode/00100-00199/00106_construct-binary-tree-from-inorder-and-postorder-traversal/Solution.ts
+++ leetcode/00100-00199/00106_construct-binary-tree-from-inorder-and-postorder-traversal/Solution.ts
@@ -3,17 +3,17 @@
  * Title: Construct Binary Tree from Inorder and Postorder Traversal
  * Difficulty: Medium
  * Description: Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3] Output: [3,9,20,null,null,15,7]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: inorder = [-1], postorder = [-1] Output: [-1]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= inorder.length <= 3000
  * postorder.length == inorder.length
  * -3000 <= inorder[i], postorder[i] <= 3000
@@ -22,12 +22,34 @@
  * inorder is guaranteed to be the inorder traversal of the tree.
  * postorder is guaranteed to be the postorder traversal of the tree.
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode";
+
+function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+  let res = null
+  if (inorder === null || inorder.length === 0) {
+    return res
+  }
+  const root = postorder[postorder.length - 1]
+  const index = inorder.indexOf(root)
+  const left = inorder.slice(0, index)
+  const right = inorder.slice(index + 1)
+  const left_postorder = postorder.slice(0, left.length)
+  const right_postorder = postorder.slice(left.length, left.length + right.length)
+  // console.log(left, right, left_postorder, right_postorder)
+  res = new TreeNode(root)
+  res.left = buildTree(left, left_postorder)
+  res.right = buildTree(right, right_postorder)
+  return res
+};
 
 function test_00106() {
-  
+  let inorder = [9, 3, 15, 20, 7], postorder = [9, 15, 7, 20, 3]
+  let res = buildTree(inorder, postorder)
+  TreeNode.print(res)
+  inorder = [1, 2], postorder = [2, 1]
+  res = buildTree(inorder, postorder)
+  TreeNode.print(res)
+
 }
 
 test_00106()
