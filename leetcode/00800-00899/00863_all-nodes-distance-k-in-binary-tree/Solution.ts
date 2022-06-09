23ba44fd918aeb4cd2c7ef86063a67f409aa18b3
geekhall <174821300@qq.com>
2022-06-09 20:40:37 +0800

    chore: add answers

--- leetcode/00800-00899/00863_all-nodes-distance-k-in-binary-tree/Solution.ts
+++ leetcode/00800-00899/00863_all-nodes-distance-k-in-binary-tree/Solution.ts
@@ -3,31 +3,93 @@
  * Title: All Nodes Distance K in Binary Tree
  * Difficulty: Medium
  * Description: Given the root of a binary tree, the value of a target node target, and an integer k, return an array of the values of all nodes that have a distance k from the target node.
- * 
+ *
  * You can return the answer in any order.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, k = 2 Output: [7,4,1] Explanation: The nodes that are a distance 2 from the target node (with value 5) have values 7, 4, and 1.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: root = [1], target = 1, k = 3 Output: []
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the tree is in the range [1, 500].
  * 0 <= Node.val <= 500
  * All the values Node.val are unique.
  * target is the value of one of the nodes in the tree.
  * 0 <= k <= 1000
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode"
+function distanceK(root: TreeNode | null, target: TreeNode | null, k: number): number[] {
+  let queue = new Array()
+
+  let resultSet = new Set()
+  let parentMap = new Map()
+  parentMap.set(root, null)
+  const markParents = (node: TreeNode | null) => {
+    if (node) {
+      if (node.left) {
+        parentMap.set(node.left, node)
+        markParents(node.left)
+      }
+      if (node.right) {
+        parentMap.set(node.right, node)
+        markParents(node.right)
+      }
+    }
+  }
+  markParents(root)
+
+  queue.push(root)
+  while (queue.length > 0) {
+    let size = queue.length
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift()
+      if (node!.left) {
+        queue.push(node!.left)
+        parentMap.set(node!.left, node)
+      }
+      if (node!.right) {
+        queue.push(node!.right)
+        parentMap.set(node!.right, node)
+      }
+    }
+  }
+
+
+  const downDistanceK = (node: TreeNode | null, distance: number) => {
+    if (node == null) return
+
+    if (distance === k && node !== target) {
+      resultSet.add(node.val)
+      result.push(node.val)
+    }
+    if (distance > k) return
+    downDistanceK(node.left, distance + 1)
+    downDistanceK(node.right, distance + 1)
+  }
+  downDistanceK(target, 0)
+
+  let current = target
+  for (let i = 0; i < k; i++) {
+    downDistanceK(current, i)
+    current = parentMap.get(current)
+  }
+  return result
+};
 
 function test_00863() {
-  
+  let root = TreeNode.create([3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]), target = root!.left, k = 2
+  console.log(distanceK(root, target, k))
+  root = TreeNode.create([1]), target = root, k = 3
+  console.log(distanceK(root, target, k))
+  root = TreeNode.create([0, 1, null, 3, 2]), target = root!.left!.right, k = 1
+  console.log(distanceK(root, target, k))
+
+
 }
 
 test_00863()
--- leetcode/00900-00999/00919_complete-binary-tree-inserter/Solution.ts
+++ leetcode/00900-00999/00919_complete-binary-tree-inserter/Solution.ts
@@ -3,33 +3,87 @@
  * Title: Complete Binary Tree Inserter
  * Difficulty: Medium
  * Description: A complete binary tree is a binary tree in which every level, except possibly the last, is completely filled, and all nodes are as far left as possible.
- * 
+ *
  * Design an algorithm to insert a new node to a complete binary tree keeping it complete after the insertion.
- * 
+ *
  * Implement the CBTInserter class:
- * 
+ *
  * CBTInserter(TreeNode root) Initializes the data structure with the root of the complete binary tree.
  * int insert(int v) Inserts a TreeNode into the tree with value Node.val == val so that the tree remains complete, and returns the value of the parent of the inserted TreeNode.
  * TreeNode get_root() Returns the root node of the tree.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input ["CBTInserter", "insert", "insert", "get_root"] [[[1, 2]], [3], [4], []] Output [null, 1, 2, [1, 2, 3, 4]] Explanation CBTInserter cBTInserter = new CBTInserter([1, 2]); cBTInserter.insert(3); // return 1 cBTInserter.insert(4); // return 2 cBTInserter.get_root(); // return [1, 2, 3, 4]
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the tree will be in the range [1, 1000].
  * 0 <= Node.val <= 5000
  * root is a complete binary tree.
  * 0 <= val <= 5000
  * At most 10 4 calls will be made to insert and get_root.
  */
-function solution() {
-  
+import { TreeNode } from "../../utils/TreeNode"
+
+class CBTInserter {
+  arr: Array<number>
+  constructor(root: TreeNode | null) {
+    this.arr = new Array()
+    let queue = new Array()
+    queue.push(root)
+    while (queue.length > 0) {
+      let cur = queue.shift()
+      this.arr.push(cur.val)
+      if (cur.left)
+        queue.push(cur.left)
+      if (cur.right)
+        queue.push(cur.right)
+    }
+  }
+
+  insert(val: number): number {
+    this.arr.push(val)
+    if (this.arr.length === 1) {
+      return -1
+    }
+    return this.arr[Math.floor((this.arr.length) / 2) - 1]
+  }
+
+  get_root(): TreeNode | null {
+    if (!this.arr || this.arr.length === 0)
+      return null
+    let treeNodeQueue = new Array<TreeNode>()
+    let numberQueue = new Array<number>()
+    for (let i = 1; i < this.arr.length; i++) {
+      numberQueue.push(this.arr[i])
+    }
+    let root = new TreeNode(this.arr[0])
+    treeNodeQueue.push(root)
+    while (numberQueue.length > 0) {
+      let leftValue = numberQueue.length > 0 ? numberQueue.shift() : null
+      let rightValue = numberQueue.length > 0 ? numberQueue.shift() : null
+      let currentNode = treeNodeQueue.shift()
+      if (leftValue !== null) {
+        let leftNode = new TreeNode(leftValue, null, null)
+        currentNode!.left = leftNode
+        treeNodeQueue.push(leftNode)
+      }
+      if (rightValue !== null) {
+        let rightNode = new TreeNode(rightValue, null, null)
+        currentNode!.right = rightNode
+        treeNodeQueue.push(rightNode)
+      }
+    }
+    return root
+  }
 }
 
 function test_00919() {
-  
+  let cBTInserter = new CBTInserter(TreeNode.create([1, 2]));
+  console.log(cBTInserter.insert(3)); // return 1
+  console.log(cBTInserter.insert(4));  // return 2
+  TreeNode.print(cBTInserter.get_root()); // return [1, 2, 3, 4]
 }
 
 test_00919()
