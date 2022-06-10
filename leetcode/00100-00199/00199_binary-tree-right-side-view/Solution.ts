8de87b302a4f8bd52583b55158b59db61b5173eb
geekhall <174821300@qq.com>
2022-06-10 13:01:17 +0800

    chore: add answers

--- leetcode/00100-00199/00199_binary-tree-right-side-view/Solution.ts
+++ leetcode/00100-00199/00199_binary-tree-right-side-view/Solution.ts
@@ -3,30 +3,53 @@
  * Title: Binary Tree Right Side View
  * Difficulty: Medium
  * Description: Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: root = [1,2,3,null,5,null,4] Output: [1,3,4]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: root = [1,null,3] Output: [1,3]
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: root = [] Output: []
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the tree is in the range [0, 100].
  * -100 <= Node.val <= 100
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode";
+function rightSideView(root: TreeNode | null): number[] {
+  let res = new Array()
+  if (!root)
+    return []
+
+  const queue = new Array<TreeNode>()
+  queue.push(root)
+  while (queue.length > 0) {
+    let size = queue.length
+    for (let i = 0; i < size; i++) {
+      let cur = queue.shift()!
+      if (cur.left)
+        queue.push(cur.left)
+      if (cur.right)
+        queue.push(cur.right)
+      if (i === size - 1) {  // most right node
+        res.push(cur.val)
+      }
+    }
+  }
+
+  return res
+};
 
 function test_00199() {
-  
+  let root = TreeNode.create([1, 2, 3, null, 5, null, 4])
+  let res = rightSideView(root)
+  console.log(res);
 }
 
 test_00199()
--- leetcode/01300-01399/01372_longest-zigzag-path-in-a-binary-tree/Solution.ts
+++ leetcode/01300-01399/01372_longest-zigzag-path-in-a-binary-tree/Solution.ts
@@ -3,41 +3,100 @@
  * Title: Longest ZigZag Path in a Binary Tree
  * Difficulty: Medium
  * Description: You are given the root of a binary tree.
- * 
+ *
  * A ZigZag path for a binary tree is defined as follow:
- * 
+ *
  * Choose any node in the binary tree and a direction (right or left).
  * If the current direction is right, move to the right child of the current node; otherwise, move to the left child.
  * Change the direction from right to left or from left to right.
  * Repeat the second and third steps until you can't move in the tree.
- * 
+ *
  * Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
- * 
+ *
  * Return the longest ZigZag path contained in that tree.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: root = [1,null,1,1,1,null,null,1,1,null,1,null,null,null,1,null,1] Output: 3 Explanation: Longest ZigZag path in blue nodes (right -> left -> right).
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: root = [1,1,1,null,1,null,null,1,1,null,1] Output: 4 Explanation: Longest ZigZag path in blue nodes (left -> right -> left -> right).
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: root = [1] Output: 0
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the tree is in the range [1, 5 * 10 4 ].
  * 1 <= Node.val <= 100
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode"
 
+function longestZigZag1(root: TreeNode | null): number {
+
+  let max = 0
+  /**
+   *
+   * @param node
+   * @param direction -1 : left  1: right
+   * @returns
+   */
+  const dfs = (node: TreeNode | null, direction: number): number => {
+    let res = 0
+    if (!node)
+      return 0
+    if (node.left === null && node.right === null) {
+      return 1
+    }
+    let left = dfs(node.left, -direction)
+    let right = dfs(node.right, -direction)
+
+    if (direction === 1) {
+      res = Math.max(left + 1, right)
+    } else {
+      res = Math.max(left, right + 1)
+    }
+    max = Math.max(max, res)
+    return res
+  }
+  dfs(root, 1)
+  dfs(root, -1)
+  return max
+};
+
+function longestZigZag(root: TreeNode | null): number {
+
+  let max = 0
+  const dfs = (node: TreeNode | null, depth: number, direction: number) => {
+    if (!node)
+      return
+
+    max = Math.max(max, depth)
+
+    if (direction === 1) {
+      dfs(node.left, depth + 1, -1)
+      dfs(node.right, 1, 1)
+    } else {
+      dfs(node.left, 1, -1)
+      dfs(node.right, depth + 1, 1)
+    }
+  }
+  dfs(root, 0, 1)
+  dfs(root, 0, -1)
+  return max
+}
 function test_01372() {
-  
+  let root = TreeNode.create([1, null, 2, 3, 4, null, null, 5, 6, null, 7, null, null, null, 8, null, 9])
+  let res = longestZigZag(root)
+  console.log(res)
+  root = TreeNode.create([1, 1, 1, null, 1, null, null, 1, 1, null, 1])
+  res = longestZigZag(root)
+  console.log(res)
+  root = TreeNode.create([1])
+  res = longestZigZag(root)
+  console.log(res)
 }
 
 test_01372()
