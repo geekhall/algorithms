d26790354955817048b02ba2fedc3c2649bfd6f6
geekhall <174821300@qq.com>
2022-06-10 11:59:53 +0800

    chore: add answer

--- leetcode/01500-01599/01530_number-of-good-leaf-nodes-pairs/Solution.ts
+++ leetcode/01500-01599/01530_number-of-good-leaf-nodes-pairs/Solution.ts
@@ -25,24 +25,61 @@
  * 1 <= distance <= 10
  */
 import { TreeNode } from "../../utils/TreeNode";
-function countPairs(root: TreeNode | null, distance: number): number {
+function countPairs1(root: TreeNode | null, distance: number): number {
   if (root === null) {
     return 0
   }
-  if (root.left === null && root.right === null) {
-    return 0
-  }
-  const left = countPairs(root.left, distance);
-  const right = countPairs(root.right, distance);
-  if (root.left === null) {
-    return right
-  }
-  if (root.right === null) {
-    return left
-  }
-  return left + right
-}
+  let count = 0
+  const dfs = (node: TreeNode | null, distance: number): number[] => {
+    if (!node)
+      return new Array(distance + 1).fill(0)
+    if (node.left === null && node.right === null) {
+      let res = new Array(distance + 1).fill(0)
+      res[1]++
+      return res
+    }
+    let left = dfs(node.left, distance)
+    let right = dfs(node.right, distance)
+    for (let l = 1; l < left.length; l++) {
+      for (let r = distance - 1; r >= 0; r--) {
+        if (l + r <= distance)
+          count += left[l] * right[r];
+      }
+    }
+    let res = new Array(distance + 1).fill(0);
+    //shift by 1
+    for (let i = res.length - 2; i >= 1; i--) {
+      res[i + 1] = left[i] + right[i];
+    }
 
+    return res;
+  }
+
+  dfs(root, distance)
+  return count
+}
+function countPairs(root: TreeNode | null, distance: number): number {
+  let count = 0
+  let dfs = (root: TreeNode | null, m = new Map()) => {
+    if (!root)
+      return m;
+    let L = dfs(root.left);
+    let R = dfs(root.right);
+    if (!root.left && !root.right)
+      m.set(1, 1); // add leaf node to map with distance 1 and count 1
+    for (let [dist1, cnt1] of L.entries())
+      for (let [dist2, cnt2] of R.entries())
+        if (dist1 + dist2 <= distance) // count "good" leaf node pairs
+          count += (cnt1 * cnt2);
+    // propagate coalesced child node maps up the recursive stack
+    // (dist + 1 because parent is dist + 1 from child)
+    for (let [dist, cnt] of L.entries()) m.set(dist + 1, cnt + (m.get(dist + 1) || 0));
+    for (let [dist, cnt] of R.entries()) m.set(dist + 1, cnt + (m.get(dist + 1) || 0));
+    return m;
+  };
+  dfs(root);
+  return count;
+}
 function test_01530() {
   let root = TreeNode.create([1, 2, 3, null, 4]), distance = 3
   console.log(countPairs(root, distance))
