8642cd3c789eeb77cf6416ec996950697b8e9251
geekhall <174821300@qq.com>
2022-06-11 22:06:55 +0800

    chore: add answers

--- leetcode/00000-00099/00095_unique-binary-search-trees-ii/Solution.ts
+++ leetcode/00000-00099/00095_unique-binary-search-trees-ii/Solution.ts
@@ -3,25 +3,51 @@
  * Title: Unique Binary Search Trees II
  * Difficulty: Medium
  * Description: Given an integer n, return all the structurally unique BST' s (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: n = 3 Output: [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: n = 1 Output: [[1]]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= n <= 8
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode";
+function generateTrees(n: number): Array<TreeNode | null> {
+  const generateSubtrees = (s: number, e: number) => {
+    let res = new Array()
+    if (s > e) {
+      res.push(null)
+      return res
+    }
+    for (let i = s; i <= e; i++) {
+      let left = generateSubtrees(s, i - 1)
+      let right = generateSubtrees(i + 1, e)
+      for (let l of left) {
+        for (let r of right) {
+          let root = new TreeNode(i)
+          root.left = l
+          root.right = r
+          res.push(root)
+        }
+      }
+    }
+    return res
+  }
+  return generateSubtrees(1, n);
+};
 
 function test_00095() {
-  
+  let n = 4
+  let res = generateTrees(n)
+  res.forEach(node => {
+    TreeNode.print(node)
+    console.log()
+  })
 }
 
 test_00095()
--- leetcode/00000-00099/00096_unique-binary-search-trees/Solution.ts
+++ leetcode/00000-00099/00096_unique-binary-search-trees/Solution.ts
@@ -3,25 +3,37 @@
  * Title: Unique Binary Search Trees
  * Difficulty: Medium
  * Description: Given an integer n, return the number of structurally unique BST' s (binary search trees) which has exactly n nodes of unique values from 1 to n.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: n = 3 Output: 5
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: n = 1 Output: 1
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= n <= 19
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode";
+// DP
+function numTrees(n: number): number {
+  let res = new Array(n + 1).fill(0)
+  res[0] = 1
+  res[1] = 1
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      res[i] += res[j - 1] * res[i - j]
+    }
+  }
+  return res[n]
+};
 
 function test_00096() {
-  
+  let n = 4
+  let res = numTrees(n)
+  console.log(res)
 }
 
 test_00096()
