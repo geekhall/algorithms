382550909dcfbac9f1b94a2107b8104cd05a593a
geekhall <174821300@qq.com>
2022-06-12 09:34:51 +0800

    chore: add answers

--- leetcode/00100-00199/00124_binary-tree-maximum-path-sum/Solution.ts
+++ leetcode/00100-00199/00124_binary-tree-maximum-path-sum/Solution.ts
@@ -3,30 +3,50 @@
  * Title: Binary Tree Maximum Path Sum
  * Difficulty: Hard
  * Description: A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
- * 
+ *
  * The path sum of a path is the sum of the node's values in the path.
- * 
+ *
  * Given the root of a binary tree, return the maximum path sum of any non-empty path.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: root = [1,2,3] Output: 6 Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: root = [-10,9,20,null,null,15,7] Output: 42 Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the tree is in the range [1, 3 * 10 4 ].
  * -1000 <= Node.val <= 1000
  */
-function solution() {
-  
-}
+import { TreeNode } from "../../utils/TreeNode"
+
+function maxPathSum(root: TreeNode | null): number {
+  let max = Number.MIN_SAFE_INTEGER
+  const dfs = (node: TreeNode | null): number => {
+    if (!node)
+      return 0
+
+    let left = Math.max(0, dfs(node.left))
+    let right = Math.max(0, dfs(node.right))
+    max = Math.max(max, left + right + node.val)
+    return Math.max(left, right) + node.val
+  }
+  dfs(root)
+  return max
+};
 
 function test_00124() {
-  
+  let root = TreeNode.create([1, 2, 3])
+  let result = maxPathSum(root)
+  console.log(result)
+
+  root = TreeNode.create([-10, 9, 20, null, null, 15, 7])
+  result = maxPathSum(root)
+  console.log(result)
+
 }
 
 test_00124()
--- leetcode/00300-00399/00331_verify-preorder-serialization-of-a-binary-tree/Solution.ts
+++ leetcode/00300-00399/00331_verify-preorder-serialization-of-a-binary-tree/Solution.ts
@@ -3,42 +3,60 @@
  * Title: Verify Preorder Serialization of a Binary Tree
  * Difficulty: Medium
  * Description: One way to serialize a binary tree is to use preorder traversal. When we encounter a non-null node, we record the node's value. If it is a null node, we record using a sentinel value such as '#'.
- * 
+ *
  * For example, the above binary tree can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#", where '#' represents a null node.
- * 
+ *
  * Given a string of comma-separated values preorder, return true if it is a correct preorder traversal serialization of a binary tree.
- * 
+ *
  * It is guaranteed that each comma-separated value in the string must be either an integer or a character '#' representing null pointer.
- * 
+ *
  * You may assume that the input format is always valid.
- * 
+ *
  * For example, it could never contain two consecutive commas, such as "1,,3".
- * 
+ *
  * Note: You are not allowed to reconstruct the tree.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#" Output: true
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: preorder = "1,#" Output: false
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: preorder = "9,#,#,1" Output: false
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= preorder.length <= 10 4
  * preorder consist of integers in the range [0, 100] and '#' separated by commas ','.
  */
-function solution() {
-  
-}
+/**
+ * In a binary tree, if we consider null as leaves, it is a full-binary tree. then
+ * 1. all non-null node provides 2 out-degree and 1 in-degree (2 children and 1 parent), except root
+ * 2. all null node provides 0 out-degree and 1 in-degree (0 child and 1 parent).
+ * @param preorder
+ * @returns
+ */
+function isValidSerialization(preorder: string): boolean {
+  let nodes = preorder.split(",")
+  let diff = 1  // 1 for root
+  for (let node of nodes) {
+    if (--diff < 0) return false; // in-degree--
+    if (node !== "#") diff += 2;  // out-degree+=2
+  }
+  return diff == 0;
+};
 
 function test_00331() {
-  
+  let preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#"
+  console.log(isValidSerialization(preorder))
+  preorder = "1,#"
+  console.log(isValidSerialization(preorder))
+  preorder = "9,#,#,1"
+  console.log(isValidSerialization(preorder))
 }
 
 test_00331()
