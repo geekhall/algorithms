e874122939c8366d3e1b7b893decce6139239643
geekhall <174821300@qq.com>
2022-06-15 22:13:49 +0800

    chore: add answers

--- leetcode/00000-00099/00024_swap-nodes-in-pairs/Solution.ts
+++ leetcode/00000-00099/00024_swap-nodes-in-pairs/Solution.ts
@@ -3,30 +3,65 @@
  * Title: Swap Nodes in Pairs
  * Difficulty: Medium
  * Description: Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: head = [1,2,3,4] Output: [2,1,4,3]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: head = [] Output: []
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: head = [1] Output: [1]
- * 
+ *
  * Constraints:
- * 
+ *
  * The number of nodes in the list is in the range [0, 100].
  * 0 <= Node.val <= 100
  */
-function solution() {
-  
-}
+import { ListNode } from "../../utils/ListNode"
+function swapPairs(head: ListNode | null): ListNode | null {
+  if (head === null)
+    return null
+  if (head.next === null)
+    return head
+  let newHead = head.next
+  let prev: ListNode | null = head
+  let curr = head.next
+  while (curr !== null && curr.next !== null) {
+    let next = curr.next
+    curr.next = prev
+    prev!.next = next.next === null ? next : next.next
+    prev = next
+    curr = next!.next!
+  }
+  if (curr !== null) {
+    curr.next = prev
+  }
+  if (prev !== null) {
+    prev.next = null
+  }
+
+  return newHead
+};
 
 function test_00024() {
-  
+  let head = ListNode.create([1, 2])
+  ListNode.print(swapPairs(head))
+  head = ListNode.create([1, 2, 3])
+  ListNode.print(swapPairs(head))
+  head = ListNode.create([1, 2, 3, 4])
+  ListNode.print(swapPairs(head))
+  head = ListNode.create([1, 2, 3, 4, 5])
+  ListNode.print(swapPairs(head))
+  head = ListNode.create([1, 2, 3, 4, 5, 6])
+  ListNode.print(swapPairs(head))
+  head = ListNode.create([1, 2, 3, 4, 5, 6, 7])
+  ListNode.print(swapPairs(head))
+  head = ListNode.create([1, 2, 3, 4, 5, 6, 7, 8])
+  ListNode.print(swapPairs(head))
 }
 
 test_00024()
--- leetcode/00700-00799/00797_all-paths-from-source-to-target/Solution.ts
+++ leetcode/00700-00799/00797_all-paths-from-source-to-target/Solution.ts
@@ -3,19 +3,19 @@
  * Title: All Paths From Source to Target
  * Difficulty: Medium
  * Description: Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
- * 
+ *
  * The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: graph = [[1,2],[3],[3],[]] Output: [[0,1,3],[0,2,3]] Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: graph = [[4,3,1],[3,2,4],[3],[4],[]] Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
- * 
+ *
  * Constraints:
- * 
+ *
  * n == graph.length
  * 2 <= n <= 15
  * 0 <= graph[i][j] < n
@@ -23,12 +23,36 @@
  * All the elements of graph[i] are unique.
  * The input graph is guaranteed to be a DAG.
  */
-function solution() {
-  
-}
+function allPathsSourceTarget(graph: number[][]): number[][] {
+  let res: number[][] = []
+  let path: number[] = []
+  let visited: boolean[] = new Array(graph.length).fill(false)
+  const dfs = (node: number) => {
+    if (node === graph.length - 1) {
+      path.push(node)
+      res.push(path.slice())
+      path.pop()
+      return
+    }
+    visited[node] = true
+    path.push(node)
+    for (let next of graph[node]) {
+      if (!visited[next]) {
+        dfs(next)
+      }
+    }
+    path.pop()
+    visited[node] = false
+  }
+  dfs(0)
+  return res
+};
 
 function test_00797() {
-  
+  let graph = [[1, 2], [3], [3], []]
+  console.log(allPathsSourceTarget(graph))
+  graph = [[4, 3, 1], [3, 2, 4], [3], [4], []]
+  console.log(allPathsSourceTarget(graph))
 }
 
 test_00797()
