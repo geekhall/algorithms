0d09c2c06c93072cbe51e472556a700c8b1d1bc5
geekhall <174821300@qq.com>
2022-06-15 23:47:00 +0800

    chore: add answers

--- leetcode/00000-00099/00064_minimum-path-sum/Solution.ts
+++ leetcode/00000-00099/00064_minimum-path-sum/Solution.ts
@@ -3,30 +3,48 @@
  * Title: Minimum Path Sum
  * Difficulty: Medium
  * Description: Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
- * 
+ *
  * Note: You can only move either down or right at any point in time.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: grid = [[1,3,1],[1,5,1],[4,2,1]] Output: 7 Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: grid = [[1,2,3],[4,5,6]] Output: 12
- * 
+ *
  * Constraints:
- * 
+ *
  * m == grid.length
  * n == grid[i].length
  * 1 <= m, n <= 200
  * 0 <= grid[i][j] <= 100
  */
-function solution() {
-  
-}
+function minPathSum(grid: number[][]): number {
+  let m = grid.length
+  let n = grid[0].length
+  // init first column
+  for (let i = 1; i < m; i++) {
+    grid[i][0] += grid[i - 1][0]
+  }
+
+  // init first row
+  for (let j = 1; j < n; j++) {
+    grid[0][j] += grid[0][j - 1]
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1])
+    }
+  }
+  return grid[m - 1][n - 1]
+};
 
 function test_00064() {
-  
+  let grid = [[1, 3, 1], [1, 5, 1], [4, 2, 1]]
+  console.log(minPathSum(grid))
 }
 
 test_00064()
--- leetcode/02000-02099/02087_minimum-cost-homecoming-of-a-robot-in-a-grid/Solution.ts
+++ leetcode/02000-02099/02087_minimum-cost-homecoming-of-a-robot-in-a-grid/Solution.ts
@@ -3,24 +3,24 @@
  * Title: Minimum Cost Homecoming of a Robot in a Grid
  * Difficulty: Medium
  * Description: There is an m x n grid, where (0, 0) is the top-left cell and (m - 1, n - 1) is the bottom-right cell. You are given an integer array startPos where startPos = [start row, start col ] indicates that initially, a robot is at the cell (start row, start col). You are also given an integer array homePos where homePos = [home row, home col ] indicates that its home is at the cell (home row, home col).
- * 
+ *
  * The robot needs to go to its home. It can move one cell in four directions: left, right, up, or down, and it can not move outside the boundary. Every move incurs some cost. You are further given two 0-indexed integer arrays: rowCosts of length m and colCosts of length n.
- * 
+ *
  * If the robot moves up or down into a cell whose row is r, then this move costs rowCosts[r].
  * If the robot moves left or right into a cell whose column is c, then this move costs colCosts[c].
- * 
+ *
  * Return the minimum total cost for this robot to return home.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: startPos = [1, 0], homePos = [2, 3], rowCosts = [5, 4, 3], colCosts = [8, 2, 6, 7] Output: 18 Explanation: One optimal path is that: Starting from (1, 0) -> It goes down to (2, 0). This move costs rowCosts[2] = 3. -> It goes right to (2, 1). This move costs colCosts[1] = 2. -> It goes right to (2, 2). This move costs colCosts[2] = 6. -> It goes right to (2, 3). This move costs colCosts[3] = 7. The total cost is 3 + 2 + 6 + 7 = 18
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: startPos = [0, 0], homePos = [0, 0], rowCosts = [5], colCosts = [26] Output: 0 Explanation: The robot is already at its home. Since no moves occur, the total cost is 0.
- * 
+ *
  * Constraints:
- * 
+ *
  * m == rowCosts.length
  * n == colCosts.length
  * 1 <= m, n <= 10 5
@@ -30,12 +30,31 @@
  * 0 <= start row, home row < m
  * 0 <= start col, home col < n
  */
-function solution() {
-  
-}
+function minCost(startPos: number[], homePos: number[], rowCosts: number[], colCosts: number[]): number {
+  let min_cost = 0
+  let i = startPos[0]
+  let j = startPos[1]
+  let x = homePos[0]
+  let y = homePos[1]
+  while (i !== x) {
+    i += (x - i) / Math.abs(x - i)
+    min_cost += rowCosts[Math.floor(i)]
+  }
+  while (j !== y) {
+    j += (y - j) / Math.abs(y - j)
+    min_cost += colCosts[Math.floor(j)]
+  }
+  return min_cost
+};
 
 function test_02087() {
-  
+  let startPos = [1, 0], homePos = [2, 3], rowCosts = [5, 4, 3], colCosts = [8, 2, 6, 7]
+  console.log(minCost(startPos, homePos, rowCosts, colCosts))
+  startPos = [5, 5], homePos = [5, 2], rowCosts = [7, 1, 3, 3, 5, 3, 22, 10, 23], colCosts = [5, 5, 6, 2, 0, 16]
+  console.log(minCost(startPos, homePos, rowCosts, colCosts))
+
+
+
 }
 
 test_02087()
