313ba42a9dd90156a12cb3469bfc0507285b4b79
geekhall <174821300@qq.com>
2022-06-15 22:45:44 +0800

    chore: add answers

--- leetcode/00500-00599/00529_minesweeper/Solution.ts
+++ leetcode/00500-00599/00529_minesweeper/Solution.ts
@@ -3,34 +3,34 @@
  * Title: Minesweeper
  * Difficulty: Medium
  * Description: Let's play the minesweeper game (Wikipedia, online game)!
- * 
+ *
  * You are given an m x n char matrix board representing the game board where:
- * 
+ *
  * 'M' represents an unrevealed mine,
  * 'E' represents an unrevealed empty square,
  * 'B' represents a revealed blank square that has no adjacent mines (i.e., above, below, left, right, and all 4 diagonals),
  * digit ('1' to '8') represents how many mines are adjacent to this revealed square, and
  * 'X' represents a revealed mine.
- * 
+ *
  * You are also given an integer array click where click = [click r, click c ] represents the next click position among all the unrevealed squares ('M' or 'E').
- * 
+ *
  * Return the board after revealing this position according to the following rules:
- * 
+ *
  * If a mine 'M' is revealed, then the game is over. You should change it to 'X'.
  * If an empty square 'E' with no adjacent mines is revealed, then change it to a revealed blank 'B' and all of its adjacent unrevealed squares should be revealed recursively.
  * If an empty square 'E' with at least one adjacent mine is revealed, then change it to a digit ('1' to '8') representing the number of adjacent mines.
  * Return the board when no more squares will be revealed.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: board = [["E","E","E","E","E"],["E","E","M","E","E"],["E","E","E","E","E"],["E","E","E","E","E"]], click = [3,0] Output: [["B","1","E","1","B"],["B","1","M","1","B"],["B","1","1","1","B"],["B","B","B","B","B"]]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: board = [["B","1","E","1","B"],["B","1","M","1","B"],["B","1","1","1","B"],["B","B","B","B","B"]], click = [1,2] Output: [["B","1","E","1","B"],["B","1","X","1","B"],["B","1","1","1","B"],["B","B","B","B","B"]]
- * 
+ *
  * Constraints:
- * 
+ *
  * m == board.length
  * n == board[i].length
  * 1 <= m, n <= 50
@@ -40,12 +40,54 @@
  * 0 <= click c < n
  * board[click r ][click c ] is either 'M' or 'E'.
  */
-function solution() {
-  
-}
+function updateBoard(board: string[][], click: number[]): string[][] {
+  let r = click[0]
+  let c = click[1]
+  if (board[r][c] === 'M') {
+    board[r][c] = 'X'
+    return board
+  }
+  let count = 0
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (i === 0 && j === 0) {
+        continue
+      }
+      if (r + i < 0 || r + i >= board.length || c + j < 0 || c + j >= board[0].length) {
+        continue
+      }
+      if (board[r + i][c + j] === 'M') {
+        count++
+      }
+    }
+  }
+  if (count > 0) {
+    board[r][c] = count.toString()
+  } else {
+    board[r][c] = 'B'
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (i === 0 && j === 0) {
+          continue
+        }
+        if (r + i < 0 || r + i >= board.length || c + j < 0 || c + j >= board[0].length) {
+          continue
+        }
+        if (board[r + i][c + j] === 'E') {
+          board = updateBoard(board, [r + i, c + j])
+        }
+      }
+    }
+  }
+  return board
+};
 
 function test_00529() {
-  
+  let board = [["E", "E", "E", "E", "E"], ["E", "E", "M", "E", "E"], ["E", "E", "E", "E", "E"], ["E", "E", "E", "E", "E"]], click = [3, 0]
+  console.log(updateBoard(board, click))
+
+  board = [["B", "1", "E", "1", "B"], ["B", "1", "M", "1", "B"], ["B", "1", "1", "1", "B"], ["B", "B", "B", "B", "B"]], click = [1, 2]
+  console.log(updateBoard(board, click))
 }
 
 test_00529()
--- leetcode/01900-01999/01992_find-all-groups-of-farmland/Solution.ts
+++ leetcode/01900-01999/01992_find-all-groups-of-farmland/Solution.ts
@@ -3,39 +3,63 @@
  * Title: Find All Groups of Farmland
  * Difficulty: Medium
  * Description: You are given a 0-indexed m x n binary matrix land where a 0 represents a hectare of forested land and a 1 represents a hectare of farmland.
- * 
+ *
  * To keep the land organized, there are designated rectangular areas of hectares that consist entirely of farmland. These rectangular areas are called groups. No two groups are adjacent, meaning farmland in one group is not four-directionally adjacent to another farmland in a different group.
- * 
+ *
  * land can be represented by a coordinate system where the top left corner of land is (0, 0) and the bottom right corner of land is (m-1, n-1). Find the coordinates of the top left and bottom right corner of each group of farmland. A group of farmland with a top left corner at (r 1, c 1) and a bottom right corner at (r 2, c 2) is represented by the 4-length array [r 1, c 1, r 2, c 2 ].
- * 
+ *
  * Return a 2D array containing the 4-length arrays described above for each group of farmland in land. If there are no groups of farmland, return an empty array. You may return the answer in any order.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: land = [[1,0,0],[0,1,1],[0,1,1]] Output: [[0,0,0,0],[1,1,2,2]] Explanation: The first group has a top left corner at land[0][0] and a bottom right corner at land[0][0]. The second group has a top left corner at land[1][1] and a bottom right corner at land[2][2].
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: land = [[1,1],[1,1]] Output: [[0,0,1,1]] Explanation: The first group has a top left corner at land[0][0] and a bottom right corner at land[1][1].
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: land = [[0]] Output: [] Explanation: There are no groups of farmland.
- * 
+ *
  * Constraints:
- * 
+ *
  * m == land.length
  * n == land[i].length
  * 1 <= m, n <= 300
  * land consists of only 0 's and 1 's.
  * Groups of farmland are rectangular in shape.
  */
-function solution() {
-  
-}
+function findFarmland(land: number[][]): number[][] {
+  let result: number[][] = []
+  for (let i = 0; i < land.length; i++) {
+    for (let j = 0; j < land[i].length; j++) {
+      if (land[i][j] === 1) {
+        let top = i
+        let left = j
+        let bottom = i
+        let right = j
+        while (bottom < land.length - 1 && land[bottom + 1][j] === 1) {
+          bottom++
+        }
+        while (right < land[i].length - 1 && land[i][right + 1] === 1) {
+          right++
+        }
+        result.push([top, left, bottom, right])
+        for (let k = top; k <= bottom; k++) {
+          for (let l = left; l <= right; l++) {
+            land[k][l] = 0
+          }
+        }
+      }
+    }
+  }
+  return result
+};
 
 function test_01992() {
-  
+  let land = [[1, 0, 0], [0, 1, 1], [0, 1, 1]]
+  console.log(findFarmland(land))
 }
 
 test_01992()
