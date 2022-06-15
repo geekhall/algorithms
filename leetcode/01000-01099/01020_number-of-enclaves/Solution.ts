4159aa08f577096a61ea8b54212964d9aebad98c
geekhall <174821300@qq.com>
2022-06-15 12:55:14 +0800

    chore: add answers

--- leetcode/01000-01099/01020_number-of-enclaves/Solution.ts
+++ leetcode/01000-01099/01020_number-of-enclaves/Solution.ts
@@ -3,32 +3,73 @@
  * Title: Number of Enclaves
  * Difficulty: Medium
  * Description: You are given an m x n binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
- * 
+ *
  * A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
- * 
+ *
  * Return the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]] Output: 3 Explanation: There are three 1s that are enclosed by 0s, and one 1 that is not enclosed because its on the boundary.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]] Output: 0 Explanation: All 1s are either on the boundary or can reach the boundary.
- * 
+ *
  * Constraints:
- * 
+ *
  * m == grid.length
  * n == grid[i].length
  * 1 <= m, n <= 500
  * grid[i][j] is either 0 or 1.
  */
-function solution() {
-  
-}
+function numEnclaves(grid: number[][]): number {
+  let m = grid.length
+  let n = grid[0].length
+  let count = 0
+  const dfs = (i: number, j: number) => {
+    if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] === 0) {
+      return
+    }
+    grid[i][j] = 0
+    dfs(i + 1, j)
+    dfs(i - 1, j)
+    dfs(i, j + 1)
+    dfs(i, j - 1)
+  }
+  for (let i = 0; i < m; i++) {
+    if (grid[i][0] === 1) {
+      dfs(i, 0)
+    }
+    if (grid[i][n - 1] === 1) {
+      dfs(i, n - 1)
+    }
+  }
+  for (let j = 0; j < n; j++) {
+    if (grid[0][j] === 1) {
+      dfs(0, j)
+    }
+    if (grid[m - 1][j] === 1) {
+      dfs(m - 1, j)
+    }
+  }
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 1) {
+        count++
+      }
+    }
+  }
+
+  return count
+};
 
 function test_01020() {
-  
+  let grid = [[0, 0, 0, 0], [1, 0, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]
+  console.log(numEnclaves(grid));
+  grid = [[0, 1, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 0, 0]]
+  console.log(numEnclaves(grid));
 }
 
 test_01020()
--- leetcode/01900-01999/01905_count-sub-islands/Solution.ts
+++ leetcode/01900-01999/01905_count-sub-islands/Solution.ts
@@ -3,32 +3,72 @@
  * Title: Count Sub Islands
  * Difficulty: Medium
  * Description: You are given two m x n binary matrices grid1 and grid2 containing only 0 's (representing water) and 1 's (representing land). An island is a group of 1 's connected 4-directionally (horizontal or vertical). Any cells outside of the grid are considered water cells.
- * 
+ *
  * An island in grid2 is considered a sub-island if there is an island in grid1 that contains all the cells that make up this island in grid2.
- * 
+ *
  * Return the number of islands in grid2 that are considered sub-islands.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: grid1 = [[1,1,1,0,0],[0,1,1,1,1],[0,0,0,0,0],[1,0,0,0,0],[1,1,0,1,1]], grid2 = [[1,1,1,0,0],[0,0,1,1,1],[0,1,0,0,0],[1,0,1,1,0],[0,1,0,1,0]] Output: 3 Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2. The 1s colored red in grid2 are those considered to be part of a sub-island. There are three sub-islands.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: grid1 = [[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]], grid2 = [[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]] Output: 2 Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2. The 1s colored red in grid2 are those considered to be part of a sub-island. There are two sub-islands.
- * 
+ *
  * Constraints:
- * 
+ *
  * m == grid1.length == grid2.length
  * n == grid1[i].length == grid2[i].length
  * 1 <= m, n <= 500
  * grid1[i][j] and grid2[i][j] are either 0 or 1.
  */
-function solution() {
-  
-}
+/**
+ * 沉掉grid2中的子岛，每次沉掉的时候，把grid2中的岛屿的值改为0，
+ *
+ * @param grid1
+ * @param grid2
+ * @returns
+ */
+function countSubIslands(grid1: number[][], grid2: number[][]): number {
+  let m = grid1.length
+  let n = grid1[0].length
+  let count = 0
+  let is_sub_island = true
+  const dfs = (i: number, j: number) => {
+    if (i < 0 || i >= m || j < 0 || j >= n || grid2[i][j] === 0) {
+      return
+    }
+    if (grid1[i][j] === 0) {
+      is_sub_island = false
+      return
+    }
+    grid2[i][j] = 0
+    dfs(i + 1, j)
+    dfs(i - 1, j)
+    dfs(i, j + 1)
+    dfs(i, j - 1)
+  }
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid1[i][j] === 1 && grid2[i][j] === 1) {
+        is_sub_island = true
+        dfs(i, j)
+        if (is_sub_island) {
+          count++
+        }
+      }
+    }
+  }
+  return count
+};
 
 function test_01905() {
-  
+  let grid1 = [[1, 1, 1, 0, 0], [0, 1, 1, 1, 1], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [1, 1, 0, 1, 1]], grid2 = [[1, 1, 1, 0, 0], [0, 0, 1, 1, 1], [0, 1, 0, 0, 0], [1, 0, 1, 1, 0], [0, 1, 0, 1, 0]]
+  console.log(countSubIslands(grid1, grid2))
+  grid1 = [[1, 0, 1, 0, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [1, 0, 1, 0, 1]], grid2 = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [0, 1, 0, 1, 0], [0, 1, 0, 1, 0], [1, 0, 0, 0, 1]]
+  console.log(countSubIslands(grid1, grid2))
 }
 
 test_01905()
