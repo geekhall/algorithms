de8b0473c6dbc65d7ce875608bacefa54d119ad8
geekhall <174821300@qq.com>
2022-06-16 22:38:24 +0800

    chore: add answers

--- leetcode/00000-00099/00045_jump-game-ii/Solution.ts
+++ leetcode/00000-00099/00045_jump-game-ii/Solution.ts
@@ -24,13 +24,27 @@
  * 0 <= nums[i] <= 1000
  */
 function jump(nums: number[]): number {
-
+  // dp[i] 表示到达 i 位置，最少需要多少步
+  let dp = new Array(nums.length).fill(0)
+  dp[0] = 0
+  for (let i = 1; i < nums.length; i++) {
+    let min_jump = nums.length
+    for (let j = 0; j < i; j++) {
+      if (nums[j] + j >= i) {
+        min_jump = Math.min(min_jump, dp[j] + 1)
+      }
+    }
+    dp[i] = min_jump
+  }
+  return dp[nums.length - 1]
 };
 
 function test_00045() {
   let nums = [2, 3, 1, 1, 4]
   console.log(jump(nums))
   nums = [2, 3, 0, 1, 4]
+  console.log(jump(nums))
+  nums = [2, 1, 1, 1, 1]
   console.log(jump(nums))
 }
 
