790042c6da3ceae318f7db0a0cfcdbeb456e542e
geekhall <174821300@qq.com>
2022-06-16 09:55:46 +0800

    chore: add answer

--- leetcode/00100-00199/00176_second-highest-salary/Solution.sql
+++ leetcode/00100-00199/00176_second-highest-salary/Solution.sql
@@ -3,27 +3,30 @@
  * Title: Second Highest Salary
  * Difficulty: Medium
  * Description: Table: Employee
- * 
+ *
  * +-------------+------+ | Column Name | Type | +-------------+------+ | id | int | | salary | int | +-------------+------+ id is the primary key column for this table. Each row of this table contains information about the salary of an employee.
- * 
+ *
  * Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
- * 
+ *
  * The query result format is in the following example.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: Employee table: +----+--------+ | id | salary | +----+--------+ | 1 | 100 | | 2 | 200 | | 3 | 300 | +----+--------+ Output: +---------------------+ | SecondHighestSalary | +---------------------+ | 200 | +---------------------+
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: Employee table: +----+--------+ | id | salary | +----+--------+ | 1 | 100 | +----+--------+ Output: +---------------------+ | SecondHighestSalary | +---------------------+ | null | +---------------------+
  */
-function solution() {
-  
-}
+create table Employee (
+	id INT(10),
+	salary int(10)
+);
+insert into Employee values (1, 100);
+insert into Employee values (2, 200);
+insert into Employee values (3, 300);
+delete from Employee
 
-function test_00176() {
-  
-}
-
-test_00176()
+SELECT max(Salary) as SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT max(Salary) FROM Employee)
