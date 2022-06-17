04a2b2b3580319b588b55064359761efbcfa935b
geekhall <174821300@qq.com>
2022-06-17 21:52:50 +0800

    chore: add answer

--- leetcode/00900-00999/00934_shortest-bridge/Solution.ts
+++ leetcode/00900-00999/00934_shortest-bridge/Solution.ts
@@ -30,12 +30,59 @@
  * There are exactly two islands in grid.
  */
 function shortestBridge(grid: number[][]): number {
-  let min_flip = Number.MAX_VALUE
   const directions = [[0, 1], [0, -1], [-1, 0], [1, 0]]
-  const expand = (grid: number[][], visited: boolean[][], i: number, j: number) => {
-    pp
+  const visited = new Array(grid.length).fill(0).map(() => new Array(grid.length).fill(false))
+  const queue: [number, number][] = []
+  /**
+   * dfs to find the first island
+   */
+  const dfs = (i: number, j: number) => {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid.length || visited[i][j] || grid[i][j] === 0) {
+      return
+    }
+    if (grid[i][j] === 1) {
+      visited[i][j] = true
+      queue.push([i, j])
+      for (let k = 0; k < directions.length; k++) {
+        dfs(i + directions[k][0], j + directions[k][1])
+      }
+    }
   }
-  return min_flip
+  const findFirstIsland = () => {
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid.length; j++) {
+        if (grid[i][j] === 1) {
+          dfs(i, j)
+          return
+        }
+      }
+    }
+  }
+  findFirstIsland()
+  // console.log(visited)
+  /**
+   * bfs to expand the first island
+   */
+  while (queue.length > 0) {
+    let cur = queue.shift()!
+    const x = cur[0]
+    const y = cur[1]
+    for (let k = 0; k < directions.length; k++) {
+      let [nx, ny] = [x + directions[k][0], y + directions[k][1]]
+      if (nx < 0 || nx >= grid.length || ny < 0 || ny >= grid.length || visited[nx][ny]) {
+        continue
+      }
+      if (grid[nx][ny] === 0) {
+        visited[nx][ny] = true
+        grid[nx][ny] = grid[x][y] + 1
+        queue.push([nx, ny])
+      } else if (grid[nx][ny] === 1 && visited[nx][ny] === false) { // reach the second island
+        return grid[x][y] - 1
+      }
+    }
+  }
+  // console.log(grid)
+  return -1
 };
 
 function test_00934() {
