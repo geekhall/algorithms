b9b05fa6d62e97c7eae7cba48d205cc37e08dbf9
geekhall <174821300@qq.com>
2022-06-18 14:12:34 +0800

    chore: add answers

--- leetcode/00700-00799/00745_prefix-and-suffix-search/Solution.ts
+++ leetcode/00700-00799/00745_prefix-and-suffix-search/Solution.ts
@@ -3,30 +3,91 @@
  * Title: Prefix and Suffix Search
  * Difficulty: Hard
  * Description: Design a special dictionary with some words that searchs the words in it by a prefix and a suffix.
- * 
+ *
  * Implement the WordFilter class:
- * 
+ *
  * WordFilter(string[] words) Initializes the object with the words in the dictionary.
  * f(string prefix, string suffix) Returns the index of the word in the dictionary, which has the prefix prefix and the suffix suffix. If there is more than one valid index, return the largest of them. If there is no such word in the dictionary, return -1.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input ["WordFilter", "f"] [[["apple"]], ["a", "e"]] Output [null, 0] Explanation WordFilter wordFilter = new WordFilter(["apple"]); wordFilter.f("a", "e"); // return 0, because the word at index 0 has prefix = "a" and suffix = 'e".
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= words.length <= 15000
  * 1 <= words[i].length <= 10
  * 1 <= prefix.length, suffix.length <= 10
  * words[i], prefix and suffix consist of lower-case English letters only.
  * At most 15000 calls will be made to the function f.
  */
-function solution() {
-  
+class TrieNode {
+  index: number
+  children: Array<TrieNode>
+  constructor() {
+    this.index = 0
+    this.children = new Array(27)
+  }
+}
+class WordFilter {
+  m: Map<string, number>
+  constructor(words: string[]) {
+    this.m = new Map()
+    for (let i = 0; i < words.length; i++) {
+      this.insert(words[i], i)
+    }
+  }
+  insert(word: string, index: number) {
+    for (let i = 0; i <= word.length; i++) {
+      for (let j = 0; j <= word.length; j++) {
+        let prefix = word.substring(0, i)
+        let suffix = word.substring(j)
+        let key = prefix + '#' + suffix
+        if (!this.m.has(key)) {
+          this.m.set(key, index)
+        } else {
+          this.m.set(key, Math.max(index, this.m.get(key)!))
+        }
+      }
+    }
+  }
+
+  f(prefix: string, suffix: string): number {
+    let key = prefix + '#' + suffix
+    if (!this.m.has(key)) {
+      return -1
+    }
+    return this.m.get(key)!
+  }
 }
 
 function test_00745() {
-  
+  let wf = new WordFilter(["apple"])
+  console.log(wf.f("a", "e"))
+  wf = new WordFilter([
+    "cabaabaaaa",
+    "ccbcababac",
+    "bacaabccba",
+    "bcbbcbacaa",
+    "abcaccbcaa",
+    "accabaccaa",
+    "cabcbbbcca",
+    "ababccabcb",
+    "caccbbcbab",
+    "bccbacbcba"]
+  )
+  // [null,9,4,5,0,8,1,2,5,3,1]
+  console.log(wf.f("bccbacbcba", "a"))  // return 9
+  console.log(wf.f("ab", "abcaccbcaa")) // return 4
+  console.log(wf.f("a", "aa"))          // return 5
+  console.log(wf.f("cabaaba", "abaaaa"))
+  console.log(wf.f("cacc", "accbbcbab"))
+  console.log(wf.f("ccbcab", "bac"))
+  console.log(wf.f("bac", "cba"))
+  console.log(wf.f("ac", "accabaccaa"))
+  console.log(wf.f("bcbb", "aa"))
+  console.log(wf.f("ccbca", "cbcababac"))
+
 }
 
 test_00745()
--- leetcode/utils/TrieTree.ts
+++ leetcode/utils/TrieTree.ts
@@ -1,11 +1,11 @@
 class TrieNode {
   pass: number
   end: number
-  next: Array<TrieNode>
+  children: Array<TrieNode>
   constructor() {
     this.pass = 0
     this.end = 0
-    this.next = new Array(26)
+    this.children = new Array(26)
   }
 }
 export class Trie {
@@ -23,13 +23,41 @@ export class Trie {
     let index = 0
     for (let i = 0; i < chs.length; i++) {
       index = chs[i].charCodeAt(0) - 'a'.charCodeAt(0)
-      if (!node.next[index]) {
-        node.next[index] = new TrieNode()
+      if (!node.children[index]) {
+        node.children[index] = new TrieNode()
       }
-      node = node.next[index]
+      node = node.children[index]
       node.pass++
     }
     node.end++
+  }
+  isPrefix(prefix: string): boolean {
+    return false
+  }
+  isSuffix(suffix: string): boolean {
+    return false
+  }
+  print() {
+    let node = this.root
+    let queue = [node]
+    let level = 0
+    while (queue.length) {
+      let size = queue.length
+      for (let i = 0; i < size; i++) {
+        let node = queue.shift()!
+
+      }
+      level++
+      let tmp = ' '
+
+      for (let i = 0; i < 26; i++) {
+        if (node.children[i]) {
+          console.log(tmp + String.fromCharCode(i + 'a'.charCodeAt(0)))
+          queue.push(node.children[i])
+          console.log("pass: ", node.pass, "end: ", node.end)
+        }
+      }
+    }
   }
 }
 
@@ -37,9 +65,9 @@ function testTrie() {
   let trie: Trie = new Trie()
   trie.insert('abc')
   trie.insert('ab')
-  trie.insert('abcd')
-  console.log(trie);
-
+  // trie.insert('abcd')
+  // trie.insert('cdef')
+  trie.print()
 }
 
 testTrie()
