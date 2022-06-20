d9de43b4bdf616bd397d06f6b2a893a83726ba82
geekhall <174821300@qq.com>
2022-06-20 13:39:55 +0800

    chore: add answers

--- leetcode/00000-00099/00020_valid-parentheses/Solution.ts
+++ leetcode/00000-00099/00020_valid-parentheses/Solution.ts
@@ -3,35 +3,58 @@
  * Title: Valid Parentheses
  * Difficulty: Easy
  * Description: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
- * 
+ *
  * An input string is valid if:
- * 
+ *
  * Open brackets must be closed by the same type of brackets.
  * Open brackets must be closed in the correct order.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s = "()" Output: true
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: s = "()[]{}" Output: true
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: s = "(]" Output: false
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= s.length <= 10 4
  * s consists of parentheses only '()[]{}'.
  */
-function solution() {
-  
-}
+function isValid(s: string): boolean {
+  let stack = []
+  for (let i = 0; i < s.length; i++) {
+    let c = s[i]
+    if (c === '(' || c === '[' || c === '{') {
+      stack.push(c)
+    } else {
+      if (stack.length === 0) {
+        return false
+      }
+      let top = stack.pop()
+      if (c === ')' && top !== '(') {
+        return false
+      }
+      if (c === ']' && top !== '[') {
+        return false
+      }
+      if (c === '}' && top !== '{') {
+        return false
+      }
+    }
+  }
+  return stack.length === 0
+};
 
 function test_00020() {
-  
+  let s = "()[]{}"
+  let result = isValid(s)
+  console.log(result)
 }
 
 test_00020()
--- leetcode/00800-00899/00820_short-encoding-of-words/Solution.ts
+++ leetcode/00800-00899/00820_short-encoding-of-words/Solution.ts
@@ -3,33 +3,41 @@
  * Title: Short Encoding of Words
  * Difficulty: Medium
  * Description: A valid encoding of an array of words is any reference string s and array of indices indices such that:
- * 
+ *
  * words.length == indices.length
  * The reference string s ends with the '#' character.
  * For each index indices[i], the substring of s starting from indices[i] and up to (but not including) the next '#' character is equal to words[i].
- * 
+ *
  * Given an array of words, return the length of the shortest reference string s possible of any valid encoding of words.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: words = ["time", "me", "bell"] Output: 10 Explanation: A valid encoding would be s = "time#bell#" and indices = [0, 2, 5 ]. words[0] = "time", the substring of s starting from indices[0] = 0 to the next '#' is underlined in " time #bell#" words[1] = "me", the substring of s starting from indices[1] = 2 to the next '#' is underlined in "ti me #bell#" words[2] = "bell", the substring of s starting from indices[2] = 5 to the next '#' is underlined in "time# bell #"
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: words = ["t"] Output: 2 Explanation: A valid encoding would be s = "t#" and indices = [0].
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= words.length <= 2000
  * 1 <= words[i].length <= 7
  * words[i] consists of only lowercase letters.
  */
-function solution() {
-  
-}
+function minimumLengthEncoding(words: string[]): number {
+  let s = new Set<string>(words)
+  for (let w of words)
+    for (let i = 1; i < w.length; ++i)
+      s.delete(w.substring(i));
+  let res = 0;
+  for (let w of s) res += w.length + 1;
+  return res;
+};
 
 function test_00820() {
-  
+  let words = ["time", "me", "bell"]
+  console.log(minimumLengthEncoding(words))
+
 }
 
 test_00820()
