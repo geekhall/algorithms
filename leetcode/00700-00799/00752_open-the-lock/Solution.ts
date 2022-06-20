0737dd900c8669d4dcb64b705be646e1d3450b47
geekhall <174821300@qq.com>
2022-06-20 17:22:38 +0800

    chore: add answers

--- leetcode/00700-00799/00752_open-the-lock/Solution.ts
+++ leetcode/00700-00799/00752_open-the-lock/Solution.ts
@@ -3,39 +3,75 @@
  * Title: Open the Lock
  * Difficulty: Medium
  * Description: You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
- * 
+ *
  * The lock initially starts at '0000', a string representing the state of the 4 wheels.
- * 
+ *
  * You are given a list of deadends dead ends, meaning if the lock displays any of these codes, the wheels of the lock will stop turning and you will be unable to open it.
- * 
+ *
  * Given a target representing the value of the wheels that will unlock the lock, return the minimum total number of turns required to open the lock, or -1 if it is impossible.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: deadends = ["0201","0101","0102","1212","2002"], target = "0202" Output: 6 Explanation: A sequence of valid moves would be "0000" -> "1000" -> "1100" -> "1200" -> "1201" -> "1202" -> "0202". Note that a sequence like "0000" -> "0001" -> "0002" -> "0102" -> "0202" would be invalid, because the wheels of the lock become stuck after the display becomes the dead end "0102".
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: deadends = ["8888"], target = "0009" Output: 1 Explanation: We can turn the last wheel in reverse to move from "0000" -> "0009".
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888" Output: -1 Explanation: We cannot reach the target without getting stuck.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= deadends.length <= 500
  * deadends[i].length == 4
  * target.length == 4
  * target will not be in the list deadends.
  * target and deadends[i] consist of digits only.
  */
-function solution() {
-  
-}
+function openLock(deadends: string[], target: string): number {
+  if (deadends.includes("0000")) return -1
+  let dead = new Set<string>(deadends)
+  if (dead.has(target)) return -1
+  let q = new Array<string>()
+  q.push("0000")
+  let visited = new Set<string>()
+  visited.add("0000")
+  let res = 0
+  while (q.length) {
+    let size = q.length
+    for (let i = 0; i < size; ++i) {
+      let cur = q.shift()!
+      if (cur === target) return res
+      for (let i = 0; i < 4; ++i) {
+        let cur_num = parseInt(cur.substring(i, i + 1))
+        let next_up = cur.substring(0, i) + ((cur_num + 1) % 10) + cur.substring(i + 1)
+        let next_down = cur.substring(0, i) + ((cur_num === 0 ? 9 : cur_num - 1) % 10) + cur.substring(i + 1)
+        if (!visited.has(next_up) && !dead.has(next_up)) {
+          visited.add(next_up)
+          q.push(next_up)
+        }
+        if (!visited.has(next_down) && !dead.has(next_down)) {
+          visited.add(next_down)
+          q.push(next_down)
+        }
+      }
+    }
+    ++res
+  }
+  return -1
+};
 
 function test_00752() {
-  
+  let deadends = ["0201", "0101", "0102", "1212", "2002"], target = "0202"
+  console.log(openLock(deadends, target))
+  deadends = ["8888"], target = "0009"
+  console.log(openLock(deadends, target))
+  deadends = ["8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"], target = "8888"
+  console.log(openLock(deadends, target))
+  deadends = ["0000"], target = "8888"
+  console.log(openLock(deadends, target))
 }
 
 test_00752()
--- leetcode/00800-00899/00847_shortest-path-visiting-all-nodes/Solution.ts
+++ leetcode/00800-00899/00847_shortest-path-visiting-all-nodes/Solution.ts
@@ -23,15 +23,97 @@
  * If graph[a] contains b, then graph[b] contains a.
  * The input graph is always connected.
  */
-function shortestPathLength(graph: number[][]): number {
+// Solution: BFS , bit mask, too slow, 10 seconds for test case
+function shortestPathLength1(graph: number[][]): number {
+  interface Node {
+    bitMask: number
+    curr: number
+    cost: number
 
+  }
+  let N = graph.length
+  let queue: Node[] = []
+  let set: Set<Node> = new Set()
+  for (let i = 0; i < N; i++) {
+    let tmp = (1 << i)
+    set.add({ bitMask: tmp, curr: i, cost: 0 })
+    queue.push({ bitMask: tmp, curr: i, cost: 1 })
+  }
+  while (queue.length) {
+    let curr = queue.shift()!
+    console.log("curr:", curr)
+    if (curr.bitMask === ((1 << N) - 1)) {
+      return curr.cost - 1
+    } else {
+      let neighbors = graph[curr.curr]
+      for (let v of neighbors) {
+        let bitMask = curr.bitMask
+        bitMask = bitMask | (1 << v)
+        let t = { bitMask: bitMask, curr: v, cost: 0 }
+        if (!set.has(t)) {
+          queue.push({ bitMask: bitMask, curr: v, cost: curr.cost + 1 })
+          set.add(t)
+        }
+      }
+    }
+  }
+  return -1
 };
+function shortestPathLength(graph: number[][]): number {
+  const n = graph.length;
+  if (n == 1) return 0;
+  let sp = Array(n).fill(0).map(() => Array(n).fill(Number.MAX_SAFE_INTEGER));
 
+  for (let i = 0; i < n; i++) {
+    for (let j of graph[i]) {
+      sp[i][j] = 1;
+    }
+  }
+
+  for (let k = 0; k < n; k++)
+    for (let i = 0; i < n; i++)
+      for (let j = 0; j < n; j++)
+        if (sp[i][k] < 10000 && sp[k][j] < 10000 &&
+          sp[i][k] + sp[k][j] < sp[i][j])
+          sp[i][j] = sp[i][k] + sp[k][j];
+
+  //scdp
+  let dp = Array(1 << n).fill(null).map(() => Array(n).fill(null));
+
+  for (let i = 0; i < n; i++) {
+    dp[1 << i][i] = 0;
+  }
+
+  let rtn = Number.MAX_SAFE_INTEGER;
+  for (let i = 1; i < (1 << n); i++)
+    for (let j = 0; j < n; j++) {
+      if (dp[i][j] != null) continue;
+      if ((i & (1 << j)) == 0) continue;
+      let ans = Number.MAX_SAFE_INTEGER;
+      for (let k = 0; k < n; k++) {
+        if (((i & (1 << k)) != 0) && sp[j][k] < 10000 && dp[(i & ~(1 << j))][k] != null) {
+          ans = Math.min(ans, sp[j][k] + dp[(i & ~(1 << j))][k]);
+
+        }
+      }
+
+      if (ans != Number.MAX_SAFE_INTEGER) {
+        dp[i][j] = ans;
+        if (i == ((1 << n) - 1))
+          rtn = Math.min(rtn, dp[i][j]);
+      }
+    }
+
+  return rtn;
+}
 function test_00847() {
   let graph = [[1, 2, 3], [0], [0], [0]]
   console.log(shortestPathLength(graph))
   graph = [[1], [0, 2, 4], [1, 3, 4], [2], [1, 2]]
   console.log(shortestPathLength(graph))
+  graph = [[1, 5], [0, 3], [3, 5], [1, 2, 5], [7], [0, 7, 6, 2, 3], [5], [5, 4, 8], [7]]
+  console.log(shortestPathLength(graph))
+
 }
 
 test_00847()
