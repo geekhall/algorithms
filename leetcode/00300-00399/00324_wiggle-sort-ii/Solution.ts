8727db39a08d6bb2dd48768f81282b85680fb4fd
geekhall <174821300@qq.com>
2022-06-21 23:25:55 +0800

    chore: add answers

--- leetcode/00300-00399/00324_wiggle-sort-ii/Solution.ts
+++ leetcode/00300-00399/00324_wiggle-sort-ii/Solution.ts
@@ -3,31 +3,57 @@
  * Title: Wiggle Sort II
  * Difficulty: Medium
  * Description: Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
- * 
+ *
  * You may assume the input array always has a valid answer.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: nums = [1,5,1,1,6,4] Output: [1,6,1,5,1,4] Explanation: [1,4,1,5,1,6] is also accepted.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: nums = [1,3,2,2,3,1] Output: [2,3,1,3,1,2]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= nums.length <= 5 * 10 4
  * 0 <= nums[i] <= 5000
  * It is guaranteed that there will be an answer for the given input nums.
- * 
+ *
  * Follow Up: Can you do it in O(n) time and/or in-place with O(1) extra space?
  */
-function solution() {
-  
-}
+function wiggleSort(nums: number[]): void {
+  let n = nums.length
+  let mid = Math.ceil(n / 2)
+  nums.sort((a, b) => a - b)
+  let left = nums.slice(0, mid)
+  let right = nums.slice(mid)
+  left.sort((a, b) => b - a)
+  right.sort((a, b) => b - a)
+  let i = 0, j = 0
+  for (let k = 0; k < n; k++) {
+    if (k % 2 === 0) {
+      nums[k] = left[i]
+      i++
+    } else {
+      nums[k] = right[j]
+      j++
+    }
+  }
+};
+
 
 function test_00324() {
-  
+  let nums = [1, 5, 1, 1, 6, 4]
+  wiggleSort(nums)
+  console.log(nums)
+  nums = [1, 3, 2, 2, 3, 1]
+  wiggleSort(nums)
+  console.log(nums) // expect [2,3,1,3,1,2]
+  nums = [1, 1, 2, 1, 2, 2, 1]
+  wiggleSort(nums)
+  console.log(nums) // expect [1,2,1,2,1,2,1]
+
 }
 
 test_00324()
--- leetcode/00900-00999/00973_k-closest-points-to-origin/Solution.ts
+++ leetcode/00900-00999/00973_k-closest-points-to-origin/Solution.ts
@@ -3,30 +3,57 @@
  * Title: K Closest Points to Origin
  * Difficulty: Medium
  * Description: Given an array of points where points[i] = [x i, y i ] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
- * 
+ *
  * The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x 1 - x 2) 2 + (y 1 - y 2) 2).
- * 
+ *
  * You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: points = [[1,3],[-2,2]], k = 1 Output: [[-2,2]] Explanation: The distance between (1, 3) and the origin is sqrt(10). The distance between (-2, 2) and the origin is sqrt(8). Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin. We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: points = [[3,3],[5,-1],[-2,4]], k = 2 Output: [[3,3],[-2,4]] Explanation: The answer [[-2,4],[3,3]] would also be accepted.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= k <= points.length <= 10 4
  * -10 4 < x i, y i < 10 4
  */
-function solution() {
-  
-}
+// 求最小的第K个或者前K个，用大根堆保存最小的K个元素
+// 求最大的第K个或者后K个，用小根堆保存最大的K个元素
+import { PriorityQueueTemplate } from '../../utils/PriorityQueue';
+
+function kClosest(points: number[][], k: number): number[][] {
+  let pq = new PriorityQueueTemplate<number[]>((a, b) => {
+    return b[0] * b[0] + b[1] * b[1] - (a[0] * a[0] + a[1] * a[1])    // Math.sqrt can be omitted
+  })
+
+  for (let i = 0; i < points.length; i++) {
+    pq.enqueue(points[i])
+    if (pq.size() > k) {
+      pq.dequeue()
+    }
+  }
+
+  let res = []
+  while (pq.size() > 0) {
+    res.push(pq.dequeue()!)
+  }
+  return res
+
+};
 
 function test_00973() {
-  
+  let points = [[1, 3], [-2, 2]], k = 1
+  console.log(kClosest(points, k))
+  points = [[3, 3], [5, -1], [-2, 4]], k = 2
+  console.log(kClosest(points, k))
+  points = [[-5, 4], [-6, -5], [4, 6]], k = 2
+  console.log(kClosest(points, k))  //[[-5,4],[4,6]]
+
+
 }
 
 test_00973()
