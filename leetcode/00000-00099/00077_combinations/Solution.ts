9574027ab033c49583fd7c585fde6ffc4c9abce5
geekhall <174821300@qq.com>
2022-06-23 20:58:13 +0800

    chore: add answers

--- leetcode/00000-00099/00077_combinations/Solution.ts
+++ leetcode/00000-00099/00077_combinations/Solution.ts
@@ -3,28 +3,75 @@
  * Title: Combinations
  * Difficulty: Medium
  * Description: Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
- * 
+ *
  * You may return the answer in any order.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: n = 4, k = 2 Output: [ [2,4], [3,4], [2,3], [1,2], [1,3], [1,4], ]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: n = 1, k = 1 Output: [[1]]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= n <= 20
  * 1 <= k <= n
  */
-function solution() {
-  
+function combine1(n: number, k: number): number[][] {
+  let res: number[][] = []
+  let cur: number[] = []
+  let i = 1
+  let j = 1
+  let count = 0
+  while (count < k) {
+    if (i > n) {
+      i = 1
+      j++
+    }
+    if (j > n) {
+      j = 1
+      i++
+    }
+    if (i === j) {
+      cur.push(i)
+      count++
+    } else {
+      cur.push(i)
+      cur.push(j)
+      count += 2
+    }
+    if (count === k) {
+      res.push(cur)
+    }
+  }
+  return res
+};
+function combine(n: number, k: number): number[][] {
+
+  const dfs = (start: number, end: number, cur: number[], res: number[][]) => {
+    if (cur.length === k) {
+      res.push(cur)
+      return
+    }
+    for (let i = start; i <= end; i++) {
+      cur.push(i)
+      dfs(i + 1, end, cur, res)
+      cur.pop()
+    }
+  }
+  const res: number[][] = []
+  dfs(1, n, [], res)
+  return res
 }
 
 function test_00077() {
-  
+  let n = 4
+  let k = 2
+  let res = combine(n, k)
+  console.log(res)
+
 }
 
 test_00077()
--- leetcode/00100-00199/00153_find-minimum-in-rotated-sorted-array/Solution.ts
+++ leetcode/00100-00199/00153_find-minimum-in-rotated-sorted-array/Solution.ts
@@ -3,42 +3,69 @@
  * Title: Find Minimum in Rotated Sorted Array
  * Difficulty: Medium
  * Description: Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
- * 
+ *
  * [4,5,6,7,0,1,2] if it was rotated 4 times.
  * [0,1,2,4,5,6,7] if it was rotated 7 times.
- * 
+ *
  * Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
- * 
+ *
  * Given the sorted rotated array nums of unique elements, return the minimum element of this array.
- * 
+ *
  * You must write an algorithm that runs in O(log n) time.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: nums = [3,4,5,1,2] Output: 1 Explanation: The original array was [1,2,3,4,5] rotated 3 times.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: nums = [4,5,6,7,0,1,2] Output: 0 Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: nums = [11,13,15,17] Output: 11 Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
- * 
+ *
  * Constraints:
- * 
+ *
  * n == nums.length
  * 1 <= n <= 5000
  * -5000 <= nums[i] <= 5000
  * All the integers of nums are unique.
  * nums is sorted and rotated between 1 and n times.
  */
-function solution() {
-  
+function findMin(nums: number[]): number {
+  let res = 0
+  let left = 0
+  let right = nums.length - 1
+  let mid = 0
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2)
+    if (nums[left] <= nums[right]) {
+      res = nums[left]
+      break
+    } else {
+      if (nums[mid] >= nums[left]) {
+        left = mid + 1
+      } else {
+        right = mid
+      }
+    }
+  }
+  return res
 }
 
 function test_00153() {
-  
+  let nums = [3, 4, 5, 1, 2]
+  console.log(findMin(nums));
+  nums = [4, 5, 6, 7, 0, 1, 2]
+  console.log(findMin(nums));
+  nums = [11, 13, 15, 17]
+  console.log(findMin(nums));
+  nums = [2, 3, 4, 5, 1]
+  console.log(findMin(nums));
+  nums = [1]
+  console.log(findMin(nums));
+
 }
 
 test_00153()
