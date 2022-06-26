c48a3adcafbe88f55cf48430256b9777d7a23f1c
geekhall <174821300@qq.com>
2022-06-26 13:04:19 +0800

    chore: add answers

--- leetcode/00000-00099/00049_group-anagrams/Solution.ts
+++ leetcode/00000-00099/00049_group-anagrams/Solution.ts
@@ -3,33 +3,48 @@
  * Title: Group Anagrams
  * Difficulty: Medium
  * Description: Given an array of strings strs, group the anagrams together. You can return the answer in any order.
- * 
+ *
  * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: strs = ["eat","tea","tan","ate","nat","bat"] Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: strs = [""] Output: [[""]]
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: strs = ["a"] Output: [["a"]]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= strs.length <= 10 4
  * 0 <= strs[i].length <= 100
  * strs[i] consists of lowercase English letters.
  */
-function solution() {
-  
-}
+function groupAnagrams(strs: string[]): string[][] {
+  let map = new Map<string, string[]>();
+  for (let str of strs) {
+    let key = str.split('').sort().join('');
+    if (map.has(key)) {
+      map.get(key)!.push(str);
+    } else {
+      map.set(key, [str]);
+    }
+  }
+  let result: string[][] = [];
+  for (let [key, value] of map) {
+    result.push(value);
+  }
+  return result;
+};
 
 function test_00049() {
-  
+  let strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+  console.log(groupAnagrams(strs))
+
 }
 
 test_00049()
--- leetcode/01000-01099/01053_previous-permutation-with-one-swap/Solution.ts
+++ leetcode/01000-01099/01053_previous-permutation-with-one-swap/Solution.ts
@@ -3,30 +3,51 @@
  * Title: Previous Permutation With One Swap
  * Difficulty: Medium
  * Description: Given an array of positive integers arr (not necessarily distinct), return the lexicographically largest permutation that is smaller than arr, that can be made with exactly one swap (A swap exchanges the positions of two numbers arr[i] and arr[j]). If it cannot be done, then return the same array.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: arr = [3,2,1] Output: [3,1,2] Explanation: Swapping 2 and 1.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: arr = [1,1,5] Output: [1,1,5] Explanation: This is already the smallest permutation.
- * 
+ *
  * Example 3:
- * 
+ *
  * Input: arr = [1,9,4,6,7] Output: [1,7,4,6,9] Explanation: Swapping 9 and 7.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= arr.length <= 10 4
  * 1 <= arr[i] <= 10 4
  */
-function solution() {
-  
-}
+// 100% 100%
+function prevPermOpt1(arr: number[]): number[] {
+  let idx = arr.length - 2, i = arr.length - 1;
+  // find the first number that is smaller than the next one from the end
+  while (idx >= 0 && arr[idx] <= arr[idx + 1]) idx--;
+  if (idx < 0)
+    return arr;
+  // find the largest arr[i] and smallest i,
+  // which makes arr[i] < arr[idx] and i > idx;
+  while (arr[i] >= arr[idx] || (i > 0 && arr[i] == arr[i - 1]))
+    i--;
+  // swap arr[i] and arr[idx]
+  let tmp = arr[i];
+  arr[i] = arr[idx];
+  arr[idx] = tmp;
+  return arr;
+};
 
 function test_01053() {
-  
+  let arr = [3, 2, 1]
+  console.log(prevPermOpt1(arr))
+  arr = [1, 1, 5]
+  console.log(prevPermOpt1(arr))
+  arr = [1, 9, 4, 6, 7]
+  console.log(prevPermOpt1(arr))
+  arr = [3, 1, 1, 3]
+  console.log(prevPermOpt1(arr))
 }
 
 test_01053()
