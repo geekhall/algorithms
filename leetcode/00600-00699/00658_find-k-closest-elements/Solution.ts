720b7710c2b48504ed0c1220d947733c2f280fa6
geekhall <174821300@qq.com>
2022-06-26 10:16:33 +0800

    chore: add answers

--- leetcode/00600-00699/00658_find-k-closest-elements/Solution.ts
+++ leetcode/00600-00699/00658_find-k-closest-elements/Solution.ts
@@ -3,33 +3,47 @@
  * Title: Find K Closest Elements
  * Difficulty: Medium
  * Description: Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
- * 
+ *
  * An integer a is closer to x than an integer b if:
- * 
+ *
  * |a - x| < |b - x|, or
  * |a - x| == |b - x| and a < b
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: arr = [1,2,3,4,5], k = 4, x = 3 Output: [1,2,3,4]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: arr = [1,2,3,4,5], k = 4, x = -1 Output: [1,2,3,4]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= k <= arr.length
  * 1 <= arr.length <= 10 4
  * arr is sorted in ascending order.
  * -10 4 <= arr[i], x <= 10 4
  */
-function solution() {
-  
-}
+// 100%, 100%
+function findClosestElements(arr: number[], k: number, x: number): number[] {
+  let left = 0, right = arr.length - k;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2)
+    if (x - arr[mid] > arr[mid + k] - x)
+      left = mid + 1;
+    else
+      right = mid;
+  }
+  return arr.slice(left, left + k);
+};
 
 function test_00658() {
-  
+  let arr = [1, 2, 3, 4, 5], k = 4, x = 3
+  console.log(findClosestElements(arr, k, x))
+  arr = [1, 2, 3, 4, 5], k = 4, x = -1
+  console.log(findClosestElements(arr, k, x))
+  arr = [1, 1, 1, 10, 10, 10], k = 1, x = 9
+  console.log(findClosestElements(arr, k, x))
 }
 
 test_00658()
--- leetcode/02200-02299/02208_minimum-operations-to-halve-array-sum/Solution.ts
+++ leetcode/02200-02299/02208_minimum-operations-to-halve-array-sum/Solution.ts
@@ -3,28 +3,47 @@
  * Title: Minimum Operations to Halve Array Sum
  * Difficulty: Medium
  * Description: You are given an array nums of positive integers. In one operation, you can choose any number from nums and reduce it to exactly half the number. (Note that you may choose this reduced number in future operations.)
- * 
+ *
  * Return the minimum number of operations to reduce the sum of nums by at least half.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: nums = [5,19,8,1] Output: 3 Explanation: The initial sum of nums is equal to 5 + 19 + 8 + 1 = 33. The following is one of the ways to reduce the sum by at least half: Pick the number 19 and reduce it to 9.5. Pick the number 9.5 and reduce it to 4.75. Pick the number 8 and reduce it to 4. The final array is [5, 4.75, 4, 1] with a total sum of 5 + 4.75 + 4 + 1 = 14.75. The sum of nums has been reduced by 33 - 14.75 = 18.25, which is at least half of the initial sum, 18.25 >= 33/2 = 16.5. Overall, 3 operations were used so we return 3. It can be shown that we cannot reduce the sum by at least half in less than 3 operations.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: nums = [3,8,20] Output: 3 Explanation: The initial sum of nums is equal to 3 + 8 + 20 = 31. The following is one of the ways to reduce the sum by at least half: Pick the number 20 and reduce it to 10. Pick the number 10 and reduce it to 5. Pick the number 3 and reduce it to 1.5. The final array is [1.5, 8, 5] with a total sum of 1.5 + 8 + 5 = 14.5. The sum of nums has been reduced by 31 - 14.5 = 16.5, which is at least half of the initial sum, 16.5 >= 31/2 = 16.5. Overall, 3 operations were used so we return 3. It can be shown that we cannot reduce the sum by at least half in less than 3 operations.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= nums.length <= 10 5
  * 1 <= nums[i] <= 10 7
  */
-function solution() {
-  
-}
+import { PriorityQueueTemplate } from '../../utils/PriorityQueue';
+// 100% 100%
+function halveArray(nums: number[]): number {
+  let sum = nums.reduce((a, b) => a + b, 0);
+  let half = sum / 2;
+  let count = 0;
+  let pq = new PriorityQueueTemplate<number>((a, b) => b - a);
+  for (let i = 0; i < nums.length; i++) {
+    pq.enqueue(nums[i]);
+  }
+  let deducted = 0;
+  while (deducted < half) {
+    let cur = pq.dequeue()!;
+    deducted += cur / 2;
+    count++;
+    pq.enqueue(cur / 2);
+  }
+  return count;
+};
 
 function test_02208() {
-  
+  let nums = [5, 19, 8, 1]
+  console.log(halveArray(nums))
+  nums = [3, 8, 20]
+  console.log(halveArray(nums))
 }
 
 test_02208()
