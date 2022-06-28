d2449c777f0c5d9d6ff544d0aff9259372fe4a32
geekhall <174821300@qq.com>
2022-06-29 03:12:35 +0800

    chore: add answers

--- leetcode/00000-00099/00090_subsets-ii/Solution.ts
+++ leetcode/00000-00099/00090_subsets-ii/Solution.ts
@@ -3,28 +3,43 @@
  * Title: Subsets II
  * Difficulty: Medium
  * Description: Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
- * 
+ *
  * The solution set must not contain duplicate subsets. Return the solution in any order.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: nums = [1,2,2] Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: nums = [0] Output: [[],[0]]
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= nums.length <= 10
  * -10 <= nums[i] <= 10
  */
-function solution() {
-  
-}
+function subsetsWithDup(nums: number[]): number[][] {
+  let result: number[][] = [[]]
+  let s = new Set()
+  for (let i = 0; i < nums.length; i++) {
+    let len = result.length
+    for (let j = 0; j < len; j++) {
+      let r = [...result[j], nums[i]].sort((a, b) => a - b)
+      if (!s.has(r.join(','))) {
+        s.add(r.join(','))
+        result.push(r)
+      }
+    }
+  }
+  return result
+};
 
 function test_00090() {
-  
+  let nums = [1, 2, 2]
+  console.log(subsetsWithDup(nums))
+  nums = [4, 4, 4, 1, 4]
+  console.log(subsetsWithDup(nums))
 }
 
 test_00090()
--- leetcode/00200-00299/00212_word-search-ii/Solution.ts
+++ leetcode/00200-00299/00212_word-search-ii/Solution.ts
@@ -3,19 +3,19 @@
  * Title: Word Search II
  * Difficulty: Hard
  * Description: Given an m x n board of characters and a list of strings words, return all words on the board.
- * 
+ *
  * Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"] Output: ["eat","oath"]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: board = [["a","b"],["c","d"]], words = ["abcb"] Output: []
- * 
+ *
  * Constraints:
- * 
+ *
  * m == board.length
  * n == board[i].length
  * 1 <= m, n <= 12
@@ -25,12 +25,57 @@
  * words[i] consists of lowercase English letters.
  * All the strings of words are unique.
  */
-function solution() {
-  
+// time limit exceeded
+function findWords(board: string[][], words: string[]): string[] {
+  const dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+  const exist = (word: string): boolean => {
+    let m = board.length, n = board[0].length
+    let visited = new Array(m).fill(0).map(() => new Array(n).fill(0))
+    const dfs = (i: number, j: number, k: number): boolean => {
+      if (k === word.length) {
+        return true
+      }
+      if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || board[i][j] !== word[k])
+        return false
+      visited[i][j] = 1
+      for (let [x, y] of dirs) {
+        if (dfs(i + x, j + y, k + 1))
+          return true
+      }
+      visited[i][j] = 0
+      return false
+    }
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        if (dfs(i, j, 0)) {
+          return true
+        }
+      }
+    }
+    return false
+  };
+  let result: string[] = []
+  for (let word of words) {
+    if (exist(word)) {
+      result.push(word)
+    }
+  }
+  return result
 }
 
 function test_00212() {
-  
+  let board = [
+    ["o", "a", "a", "n"],
+    ["e", "t", "a", "e"],
+    ["i", "h", "k", "r"],
+    ["i", "f", "l", "v"]
+  ], words = ["oath", "pea", "eat", "rain"]
+  console.log(findWords(board, words))
+  board = [
+    ["a", "b"],
+    ["c", "d"]
+  ], words = ["abcb"]
+  console.log(findWords(board, words))
 }
 
 test_00212()
