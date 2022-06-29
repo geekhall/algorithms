a22920fd6ed623181b3054c0c1cb4d2f77f6efe5
geekhall <174821300@qq.com>
2022-06-29 22:03:25 +0800

    chore: add answers

--- leetcode/00400-00499/00424_longest-repeating-character-replacement/Solution.ts
+++ leetcode/00400-00499/00424_longest-repeating-character-replacement/Solution.ts
@@ -3,29 +3,33 @@
  * Title: Longest Repeating Character Replacement
  * Difficulty: Medium
  * Description: You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
- * 
+ *
  * Return the length of the longest substring containing the same letter you can get after performing the above operations.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: s = "ABAB", k = 2 Output: 4 Explanation: Replace the two 'A's with two 'B's or vice versa.
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: s = "AABABBA", k = 1 Output: 4 Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA". The substring "BBBB" has the longest repeating letters, which is 4.
- * 
+ *
  * Constraints:
- * 
+ *
  * 1 <= s.length <= 10 5
  * s consists of only uppercase English letters.
  * 0 <= k <= s.length
  */
-function solution() {
-  
-}
+function characterReplacement(s: string, k: number): number {
+  let m = s.length
+
+};
 
 function test_00424() {
-  
+  let s = "ABAB", k = 2
+  console.log(characterReplacement(s, k));
+  s = "AABABBA", k = 1
+  console.log(characterReplacement(s, k));
 }
 
 test_00424()
