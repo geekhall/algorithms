6fa8a03b07ea34e2a4c013fd580671974f0d530f
geekhall <174821300@qq.com>
2022-07-02 00:10:38 +0800

    chore: add answers

--- leetcode/00200-00299/00238_product-of-array-except-self/Solution.py
+++ leetcode/00200-00299/00238_product-of-array-except-self/Solution.py
@@ -30,7 +30,17 @@
 
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        pass
+        if not nums:
+            return []
+        n = len(nums)
+        res = [1] * n
+        for i in range(1, n):
+            res[i] = res[i - 1] * nums[i - 1]
+        right = 1
+        for i in range(n - 1, -1, -1):
+            res[i] *= right
+            right *= nums[i]
+        return res
 
 
 def test_00238():
--- leetcode/00200-00299/00238_product-of-array-except-self/Solution.ts
+++ leetcode/00200-00299/00238_product-of-array-except-self/Solution.ts
@@ -3,33 +3,46 @@
  * Title: Product of Array Except Self
  * Difficulty: Medium
  * Description: Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
- * 
+ *
  * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
- * 
+ *
  * You must write an algorithm that runs in O(n) time and without using the division operation.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: nums = [1,2,3,4] Output: [24,12,8,6]
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: nums = [-1,1,0,-3,3] Output: [0,0,9,0,0]
- * 
+ *
  * Constraints:
- * 
+ *
  * 2 <= nums.length <= 10 5
  * -30 <= nums[i] <= 30
  * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
- * 
+ *
  * Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
  */
-function solution() {
-  
-}
+function productExceptSelf(nums: number[]): number[] {
+  const len = nums.length;
+  const res = new Array(len);
+  res[0] = 1;
+  for (let i = 1; i < len; i++) {
+    res[i] = res[i - 1] * nums[i - 1];
+  }
+  let right = 1;
+  for (let i = len - 1; i >= 0; i--) {
+    res[i] *= right;
+    right *= nums[i];
+  }
+  return res;
+};
 
 function test_00238() {
-  
+  const nums = [1, 2, 3, 4];
+  const res = productExceptSelf(nums);
+  console.log(res);
 }
 
 test_00238()
