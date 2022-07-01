fe0447380c1b858d27bd4949f80045c87969b475
geekhall <174821300@qq.com>
2022-07-01 11:57:33 +0800

    chore: add answers

--- leetcode/00400-00499/00438_find-all-anagrams-in-a-string/Solution.py
+++ leetcode/00400-00499/00438_find-all-anagrams-in-a-string/Solution.py
@@ -1,32 +1,38 @@
-/**
- * ID:    00438
- * Title: Find All Anagrams in a String
- * Difficulty: Medium
- * Description: Given two strings s and p, return an array of all the start indices of p 's anagrams in s. You may return the answer in any order.
- *
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+'''
+ ID:    00438
+ Title: Find All Anagrams in a String
+ Difficulty: Medium
+ Description: Given two strings s and p, return an array of all the start indices of p 's anagrams in s. You may return the answer in any order.
+ * 
  * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
- *
+ * 
  * Example 1:
- *
+ * 
  * Input: s = "cbaebabacd", p = "abc" Output: [0,6] Explanation: The substring with start index = 0 is "cba", which is an anagram of "abc". The substring with start index = 6 is "bac", which is an anagram of "abc".
- *
+ * 
  * Example 2:
- *
+ * 
  * Input: s = "abab", p = "ab" Output: [0,1,2] Explanation: The substring with start index = 0 is "ab", which is an anagram of "ab". The substring with start index = 1 is "ba", which is an anagram of "ab". The substring with start index = 2 is "ab", which is an anagram of "ab".
- *
+ * 
  * Constraints:
- *
+ * 
  * 1 <= s.length, p.length <= 3 * 10 4
  * s and p consist of lowercase English letters.
- */
-function findAnagrams(s: string, p: string): number[] {
+'''
 
-};
 
-function test_00438() {
-  let s = "cbaebabacd", p = "abc"
-  console.log(findAnagrams(s, p))
-  s = 
-}
+def solution():
+    print("solution")
+    pass
 
-test_00438()
+
+def test_00438():
+    solution()
+    pass
+
+
+if __name__ == '__main__':
+    test_00438()
+
